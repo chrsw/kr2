684f522fc431a288dab179a0b16781aae4f9817b
Chris Williams <chrsw@users.noreply.github.com>
2022-12-23 09:26:25 -0500

    Add chatper 1, exercise 17.

---
+++ ch1/ex1-17.c
@@ -0,0 +1,47 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex1-17.c
+ *      Write a program to print all input lines that are longer than 80
+ *      characters.
+ */
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getlin(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+int main() {
+
+    int len;                /* current line length */
+    int max;                /* maximum length seen so far */
+    char line[MAXLINE];     /* current input line */
+    //char longest[MAXLINE];  /* longest line saved here */
+
+    max = 0;
+    while ((len = getlin(line, MAXLINE)) > 0 )
+        if (len > 80) {
+            printf("%s", line);
+        }
+
+    return 0;
+}
+
+/* getlin: read a line into s, return length
+ * getline() is now a C Standard Library function declared in
+ * stdio.h
+ */
+int getlin(char s[], int lim) {
+
+    int c, i;
+
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
---
+++ ch1/ex1-17a.c
@@ -0,0 +1,60 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex1-17a.c
+ *	    Print the longest input line.
+ */
+
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getlin(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
+int main() {
+
+    int len;                /* current line length */
+    int max;                /* maximum length seen so far */
+    char line[MAXLINE];     /* current input line */
+    char longest[MAXLINE];  /* longest line saved here */
+
+    max = 0;
+    while ((len = getlin(line, MAXLINE)) > 0 )
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+
+    if (max > 0)            /* there was a line */
+        printf("%s", longest);
+
+    return 0;
+}
+
+/* getlin: read a line into s, return length
+ * getline() is now a C Standard Library function declared in
+ * stdio.h
+ */
+int getlin(char s[], int lim) {
+
+    int c, i;
+
+    for (i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        s[i] = c;
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+/* copy: copy 'from' into 'to'; assume to is big enough */
+void copy(char to[], char from[]) {
+
+    int i;
+
+    i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
