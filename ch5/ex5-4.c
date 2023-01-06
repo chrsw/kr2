b3b4f8fd0f89f7b1ccf2d6f205e49667ebd45df2
Chris Williams <chrsw@users.noreply.github.com>
2023-01-05 21:59:25 -0500

    Add chapter 5, exercise 4.

---
+++ ch5/ex5-4.c
@@ -0,0 +1,38 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex5-4.c
+ *      Write the function strend(s,t), which returns 1 if the string t
+ *      occurs at the end of the string s, and 0 otherwise.
+ *
+ * Description:
+ *
+ * Build:
+ *      strend() is implemented in strend.c.
+ *      gcc -Wall -Wextra -o ex5-3 strcatp.c ex5.3.c
+ *
+ * Input:
+ *
+ */
+
+#include <stdio.h>
+
+int strend(char s[], char t[]);
+
+int main(int argc, char *argv[])
+{
+    (void)argc;         /* supress warnings about unused vars */
+    (void)argv;
+
+    /* test input strings */
+    char s[40] = {'s', 't', 'a', 'r', 't', ' ', 'e', 'n', 'd', '\0'};
+    //char t[40] = {'e', 'n', 'd', '\0'};
+    char t[40] = {'!', '\0'};
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+    if (strend(s,t))
+        printf("found \"%s\" at the end of \"%s\"!\n", t, s);
+
+	return 0;
+}
+
---
+++ ch5/strend.c
@@ -0,0 +1,34 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * strend.c
+ *
+ */
+
+#include <string.h>
+
+/* strend: test for t at the end of s */
+
+int strend(char s[], char t[]) {
+
+    int i = 0;
+    char *sp;
+    char *tp;
+
+    if (!s || !t)
+        return 0;	
+
+    if (strlen(t) > strlen(s))
+        return 0;
+
+    sp = s;
+    tp = t;
+
+	while ( *++sp != '\0')          /* find end of s */
+		;                           
+	while ( *++tp != '\0')          /* find end of t */
+		i++;                           
+    while (--i)                     /* start comparing the end of s*/
+        if (*--sp != *--tp)         /* with the end of t and work */
+            break                   /* backwards */;
+    return (!i);
+}
