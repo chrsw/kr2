6e6eb819744130cb476109e25413c04de27547c1
Chris Williams <chrsw@users.noreply.github.com>
2023-01-08 12:30:12 -0500

    Improve getch, getword main driver.

--- ch6/words.c
+++ ch6/words.c
@@ -5,13 +5,26 @@
  *
  * Description:
  *      Related to chapter 6, exercise 1.
+ *      This file includes a main() driver that shows example
+ *      uses of the functions getch(), ungetch(), getword() from
+ *      K&R2 chapters 4 & 6. This file does not have examples
+ *      of solutions to the exercises in chapters 4 or 6.
  *
  * Input:
+ *      List of words from input file:
+ *      $ ./words < words.txt
+ *
+ * Building:
+ *      $ gcc -o words words.c getch.c getword.c bgetword.c
  *
  */
 
 #include <stdio.h>
+#include <string.h>
 
+#define WORDSIZE 80
+
+/* External functions from getch.c and getword.c */
 int getch(void);
 int getchb(void);
 int getword(char *word, int lim);
@@ -24,15 +37,16 @@ size_t flen;
 int main(int argc, char *argv[])
 {
     int c;
+    char word[WORDSIZE];
     FILE *f = fopen("words.txt", "r");
-    char word[80];
-
-    for (c = 0; c < 80; c++) 
-        word[c] = '0';
+    (void)argc;
+    (void)argv;
+    
+    memset(word, '0', sizeof(word));
     
     if (!f)
         return 1;
-    flen = fread(buf, sizeof(char), BUFSIZ, f);     /* copy file contents */
+    flen = fread(buf, sizeof(word), BUFSIZ, f);     /* copy file contents */
                                                     /* into buffer */
 
     buf[flen++] = '\0';                             /* make sure buf */
@@ -40,22 +54,24 @@ int main(int argc, char *argv[])
 
     printf("BUFSIZ:      %d\n", BUFSIZ);            /* system buf size */
     printf("file length: %zu\n", flen);             /* data read in */
+    printf("WORDSIZE:    %d\n", WORDSIZE);
+    printf("word length: %zu\n", sizeof(word));
     
-    while (c = getchar() != EOF)
+    while ((c = getchar()) != EOF)
         ;
     bufp = 0;
-    while (c = getchb() != '\0');
+    while ((c = getchb()) != '\0');
         ;
    
     bufp = 0;
-    while (c = buf[bufp++] != '\0');
+    while ((c = buf[bufp++]) != '\0');
         ;
     
     printf("bufp:        %d\n", bufp);
  
     bufp = 0;
 
-    while (getwordb(word, BUFSIZ) != '\0')
+    while ((getwordb(word, BUFSIZ)) != '\0')
         printf("%s\n", word);
 
     printf("done!\n");
