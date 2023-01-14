00b229121f02d749646e4cf63efc84cfbc8375e6
Chris Williams <chrsw@users.noreply.github.com>
2023-01-13 23:20:11 -0500

    Add ch 1, ex 7. Print value of EOF.

---
+++ ch1/ch1-7.c
@@ -0,0 +1,19 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex1-7.c
+ *      Write a program to print the value of EOF.
+ *
+ * Description:
+ *
+ *
+ * Input:
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("The value of EOF: 0x%x\n", (unsigned int)EOF);
+    return 0;
+}
