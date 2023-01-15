cbd399b814919432b2463de236ce8c3aa5b45ee9
Chris Williams <chrsw@users.noreply.github.com>
2023-01-14 21:54:23 -0500

    Add solution for chpater 7, exercise 1.

---
+++ ch7/ex7-1.c
@@ -0,0 +1,61 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex7-1.c
+ *      Write a program that converts upper case to lower or lower to upper
+ *      depending on the name it is invoked with, as found in argv[0].
+ *
+ * Description:
+ *
+ * Input:
+ *
+ * Returns:
+ *      -1 Bad input
+ *       1 lower -> upper conversion
+ *       2 upper -> lower conversion
+ *
+ * Build:
+ *      Compile and create a binary named "ex7-1".
+ *      gcc -Wall -Wextra -o ex7-1 ex7-1.c
+ *
+ * Setup:
+ *      Create hard links with different names pointing to the same
+ *      binary.
+ *      ln ex7-1 upper && ln ex7-1 lower
+ *
+ * Run:
+ *      Run either hard link with the name of the conversion you want.
+ *      ./upper < test.txt
+ *      ./lower < test.txt
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    int conv_type = 0;
+    int c;
+
+    if (argc != 1)
+        return -1;
+    
+    if (!strcmp(argv[0], "./upper"))        /* Find the program name */
+        conv_type = 1;
+    else if (!strcmp(argv[0], "./lower"))
+        conv_type = 2;
+    else
+        return -1;
+
+    while ((c = getchar()) != EOF) {        /* Convert case based on */
+        if (conv_type == 1)                 /* program name. */
+            putchar(toupper(c));
+        else if (conv_type == 2)
+            putchar(tolower(c));
+        else
+            putchar(c);
+    }
+    
+    return conv_type;
+}
