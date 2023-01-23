0390a60aa5d9a9a0ea08be5aaac99342f5f3e05d
Chris Williams <chrsw@users.noreply.github.com>
2023-01-22 23:04:18 -0500

    Fix formatting for chapter 1, exercise 3.

--- ch1/ex1-3.c
+++ ch1/ex1-3.c
@@ -1,12 +1,16 @@
-/* 
- * ex1-3.c	Modify the temperature conversion program to print a heading
+/* vim:ts=4:sw=4:et:
+ *
+ * ex1-3.c	
+ *      Modify the temperature conversion program to print a heading
  * 		above the table.
+ *
  */
 
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table 
-    for fahr = 0, 20, ..., 300; floating-point conversion */ 
+/* print Fahrenheit-Celsius table w/ heading
+ * for fahr = 0, 20, ..., 300; floating-point conversion 
+ */ 
 
 int main(void)
 {
@@ -15,15 +19,16 @@ int main(void)
 	int lower, upper, step;
 
 	lower = 0;		/* lower limit of temperature table */
-	upper = 300;		/* upper limit */
+	upper = 300;	/* upper limit */
 	step = 20;		/* step size */
 
 	fahr = lower;
 
 	printf("%3.3s %6.6s\n", "F" ,"C"); /* print the heading with the
-					      same spacing and justification
-					      as the table */
+					                      same spacing and justification
+					                      as the table */
 
+    /* iterate through the list of fahrenheit values and convert to celcius */
 	while (fahr <= upper) {
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
