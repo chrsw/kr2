0406f5642424d8b5de0ec1892fe00ea80b98ac13
Chris Williams <chrsw@users.noreply.github.com>
2023-02-04 18:43:22 -0500

    Add chapter 1, exercise 4. Celsius to Fahrenheit conversion.

---
+++ ch1/ex1-4.c
@@ -0,0 +1,38 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ex1-3.c	
+ *      Write a program to print the corresponding Celsius to Fahrenheit
+ *      table. (See ex1-3).
+ *
+ */
+
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table w/ heading
+ * for fahr = 0, 20, ..., 300; floating-point conversion 
+ */ 
+
+int main(void)
+{
+
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;		/* lower limit of temperature table */
+	upper = 300;	/* upper limit */
+	step = 20;		/* step size */
+
+	celsius = lower;
+
+	printf("%3.3s %6.6s\n", "C" ,"F"); /* print the heading with the
+					                      same spacing and justification
+					                      as the table */
+
+    /* iterate through the list of Celsius values and convert to Fahrenheit */
+	while (celsius <= upper) {
+		fahr = (9.0/5.0) * (celsius+32.0);
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+
+}
