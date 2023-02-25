9e1edca70a5fdb85b4c3e360234c17a730587932
Chris Williams <chrsw@users.noreply.github.com>
2023-02-24 22:00:20 -0500

    Add excerpt from Chapter 8, section 2.
    Small program which demonstrates a simple input to output copy where system
    calls are used directly to perform the copy. Input is taken from standard
    input only and output is sent to standard output only. The example from
    the book uses a header file "syscalls.h" which isn't used here.

---
+++ ch8/ll_cp.c
@@ -0,0 +1,33 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ll_cp.c
+ *      Example of low level copy using syscalls instead of std lib functions.
+ *
+ * Description:
+ *      Rewrite of a file copy program from chpater 1.
+ *      Taken from K&R2 Chpater 8, section 2 Low Level I/O - Read and Write
+ *
+ * Input:
+ *      Anything on standard input.
+ *
+ * Output:
+ *      All output sent to standard output.
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)      /* copy input to output */
+{
+    char buf[BUFSIZ];
+    int n;
+
+    while ((n = read(0, buf, BUFSIZ)) > 0)
+        write(1, buf, n);
+
+    return 0;
+}
