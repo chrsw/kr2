4343aad9aee96e06f96ae92a528d90a42b8f2fb6
Chris Williams <chrsw@users.noreply.github.com>
2023-03-31 23:21:58 -0400

    Add unicode character printing template.

---
+++ ch1/unicode.c
@@ -0,0 +1,15 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * unicode.c
+ *      Print a unicode character. 
+ *
+ */
+
+#include <stdio.h>
+
+/* Unicode character printing template */
+int main(int argc, char *argv[])
+{
+    printf("\u2588\n");
+    return 0;
+}
