4f076515c297b859ad0998189d7364cd131cbcf2
Chris Williams <chrsw@users.noreply.github.com>
2023-04-08 19:37:46 -0400

    Add simple example of sweeping through colors.

---
+++ ch1/colors.c
@@ -0,0 +1,27 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * colors.c
+ *      Print a short table of numbers sweeping through colors.
+ *
+ * Input:
+ *      None.
+ *
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, n;
+
+    for (i = 0; i < 11; i++) {
+        for (j = 0; j < 10; j++) {
+            n = 10 * i + j;
+            if (n > 108) break;
+            printf("\033[%dm %3d\033[m", n, n);
+        }
+        printf("\n");
+    }
+    return 0;
+}
