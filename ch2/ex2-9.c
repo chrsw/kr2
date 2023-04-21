5d18899a5f593b3091d37a54b1341cea28959276
Chris Williams <chrsw@users.noreply.github.com>
2023-04-21 16:50:23 -0400

    Add solution to chapter 2, exercise 9.
    Use the 'x &= (x-1)' trick for removing the rightmost '1' bit in x
    to count the number of '1' bits in x, speeding up bitcount() by
    avoiding costly shift operations. TODO: benchmark this to see if there's
    any actual speed up on common, modern hardware.

---
+++ ch2/ex2-9.c
@@ -0,0 +1,36 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex2-9.c
+ *      Rewrite of bitcount() with knowing that 'x &= (x-1)' removes
+ *      the rightmost bit in x.
+ *
+ * Description:
+ *      Count 1 bits in x.
+ *      Modified excerpt from K&R2 section 2.10.
+ *
+ * Input:
+ *      Unsigned integer to count bits it contains.
+ *
+ * Build:
+ *      gcc -c ex2-9.c
+ * 
+ *
+ */
+
+
+/* bitcount: count 1 bits in x */
+int bitcount(unsigned int x) {
+
+    int b = 0;
+
+    /* shift every bit until all '1' bits are removed */    
+    //for (b = 0; x != 0; x >>= 1)
+    //    if (x & 01)             /* least significant bit being one adds */ 
+    //        b++;                /* to a running count */
+
+    while (x) {
+        x &= (x-1);
+        b++;
+    }
+    return b;
+}
