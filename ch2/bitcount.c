3e01c1dfb57be3ae68eef951b09b8f923b04c388
Chris Williams <chrsw@users.noreply.github.com>
2023-04-21 15:58:04 -0400

    Add bit counting excerpt from 2.10.

---
+++ ch2/bitcount.c
@@ -0,0 +1,29 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * bitcount.c
+ *      Count 1 bits in x.
+ *
+ * Description:
+ *      Excerpt from K&R2 section 2.10.
+ *
+ * Input:
+ *      Unsigned integer to count bits it contains.
+ *
+ * Build:
+ *      gcc -c bitcount.c
+ * 
+ *
+ */
+
+
+/* bitcount: count 1 bits in x */
+int bitcount(unsigned int x) {
+
+    int b;
+
+    /* shift every bit until all '1' bits are removed */    
+    for (b = 0; x != 0; x >>= 1)
+        if (x & 01)             /* least significant bit being one adds */ 
+            b++;                /* to a running count */
+    return b;
+}
---
+++ ch2/bitcount_test.c
@@ -0,0 +1,31 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * bitcount_test.c
+ *      Test the functionality of the bitcount() function.
+ *
+ * Description:
+ *      Template file for solutions to exercises in The C Programming
+ *      Language 2nd Ed. A longer description of the program goes here.
+ *
+ * Input:
+ *      Describe the expected input.
+ *
+ * Build:
+ *      $ gcc -o bitcount_test bitcount.c bitcount_test.c
+ *
+ */
+
+#include <stdio.h>
+
+int bitcount(unsigned int);
+
+int main(int argc, char *argv[])
+{
+    unsigned int b = 0xFDB97531;
+    printf("b = %8x\tbits set = %d\n", b, bitcount(b));
+
+    b = 0xECA87531;
+    printf("b = %8x\tbits set = %d\n", b, bitcount(b));
+
+    return 0;
+}
