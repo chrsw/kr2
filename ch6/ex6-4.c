71a08c1d88d69a040f77f59d39e9abceb0e4cda9
Chris Williams <chrsw@users.noreply.github.com>
2023-04-29 21:42:26 -0400

    Add skeleton file for chapter 6, exercise 4.

---
+++ ch6/ex6-4.c
@@ -0,0 +1,23 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex6-4.c
+ *     Print words in order of frequency. 
+ *
+ * Description:
+ *      Write a program that prints the distinct words in its input sorted
+ *      into decreasing order of frequency of occurance. Precede each word
+ *      by its count.
+ *
+ * Input:
+ *      List of words on stdin.
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+    (void)arcg;                 /* program command line is unused */
+    (void)argv;
+
+	return 0;
+}
