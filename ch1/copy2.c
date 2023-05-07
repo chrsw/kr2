180f6a35aacbd66ab39a3d4ae602d5f5255d21dc
Chris Williams <chrsw@users.noreply.github.com>
2023-05-07 18:07:45 -0400

    Add 2nd version of copy from section 1.5.1.
    More compact version of this simple program that just copies input
    to output.

---
+++ ch1/copy2.c
@@ -0,0 +1,26 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * copy2.c
+ *      Copy input to output; 2nd version.
+ *
+ * Description:
+ *  	2nd copy example from section 1.5.1
+ *
+ * Build and use:
+ * 	$ gcc -o copy copy2.c
+ * 	$ ./copy2 < copy2.c
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int c;
+	
+	c = getchar();
+	while ((c = getchar()) != EOF) {
+		putchar(c);
+	}
+
+	return 0;
+}
