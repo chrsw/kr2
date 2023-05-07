c6d59f0697d025c32825afe7a7068aa126e64338
Chris Williams <chrsw@users.noreply.github.com>
2023-05-07 18:33:29 -0400

    Add solution to chapter 1, exercise 6.
    Use printf() to show the possible results of getchar() != EOF.

---
+++ ch1/ex1-6.c
@@ -0,0 +1,23 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex1-6.c
+ *      Verify that the expression getchar() != EOF is 0 or 1.
+ *
+ * Description:
+ *
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    int c;
+
+    while ( c = (getchar() != EOF)) {       /* print the result of */
+        printf("%d", c);                  /* the comparison every read */
+    }                                       
+    printf("%d\n", c);
+
+    return 0;
+}
