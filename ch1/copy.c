2b626c6f2acbe77c507208d08bbedac876f00b13
Chris Williams <chrsw@users.noreply.github.com>
2023-05-06 23:08:03 -0400

    Add copy example 1st version from section 1.5.1.

---
+++ ch1/copy.c
@@ -0,0 +1,27 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * copy.c
+ *      Copy input to output; 1st version.
+ *
+ * Description:
+ *  	Example from section 1.5.1
+ *
+ * Build and use:
+ * 	$ gcc -o copy copy.c
+ * 	$ ./copy < copy.c
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int c;
+	
+	c = getchar();
+	while (c != EOF) {
+		putchar(c);
+		c = getchar();
+	}
+
+	return 0;
+}
