330e0869b52bf0229a4d43754c5c2d2a711ab1c8
Chris Williams <chrsw@users.noreply.github.com>
2023-05-12 22:07:38 -0400

    Add solution to chapter 1, exercise 7.
    Show the unsigned integer value of 'EOF' from C standard library.

---
+++ ch1/ex1-7.c
@@ -0,0 +1,14 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex1-7.c
+ *      Write a program to print the value of EOF.
+ *
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("EOF = %u\n", EOF);
+    return 0;
+}
