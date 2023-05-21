69fa19e78f38294f46f5d7cd7ccfd0d7f556966a
Chris Williams <chrsw@users.noreply.github.com>
2023-05-20 20:09:40 -0400

    Fix big in binary search example. Add binary search test driver.

--- ch3/binsearch.c
+++ ch3/binsearch.c
@@ -7,23 +7,23 @@
  *      An example form K&R 2 section 3.3.
  *
  * Input:
- *      Sorted array.
+ *      Sorted array, value to search for, length of array.
  *
  * Output:
  *      Index of matching element in array, -1 if no match.
  *
  */
 
-/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+/* binsearch: find x in v, where v: v[0] <= v[1] <= ... <= v[n-1] */
 int binsearch(int x, int v[], int n)
 {
     int low, high, mid;
 
     low = 0;
-    high = n -1;
+    high = n - 1;
     
     while (low <= high) {
-        mid = (low <= high) / 2;
+        mid = (low + high) / 2;
         if (x < v[mid])
             high = mid - 1;
         else if (x > v[mid])
---
+++ ch3/binstest.c
@@ -0,0 +1,31 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *      Vim modeline for consistent editor settings across files.
+ *
+ * binstest.c
+ *      Test driver for binsearch.c.
+ *
+ * Description:
+ *      Create a sorted array and pass to binsearch() to find a key.
+ * Input:
+ *      Describe the expected input.
+ *
+ * Other info:
+ *      Returns, build, notes, etc. 
+ *
+ */
+
+#include "binsearch.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    int v[] = {0,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987};
+    unsigned int n = sizeof(v)/sizeof(v[0]);
+    int key, pos; 
+    
+    key = 21;
+    pos = binsearch(key,v,16);
+
+    printf("pos = %d\n", pos);
+    return 0;
+}
