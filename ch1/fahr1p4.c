31fe33ff4b4760ef799f965283aae74ece839b62
Chris Williams <chrsw@users.noreply.github.com>
2023-05-25 23:58:24 -0400

    Add Fahrenheit-Celsius conversion from section 1.4

---
+++ ch1/fahr1p4.c
@@ -0,0 +1,30 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * fahr1_4.c
+ *      Fahrenheit-Celsius table from section 1.4.
+ *
+ * Description:
+ *
+ * Input:
+ *      Describe the expected input.
+ *
+ * Other info:
+ *      Returns, build, notes, etc. 
+ *
+ */
+
+#include <stdio.h>
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+int main(int argc, char *argv[]) {
+
+    int fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    return 0;
+}
