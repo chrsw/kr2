14169495373cf0f5e93add5fe9580c7c30cabbe0
Chris Williams <chrsw@users.noreply.github.com>
2023-05-30 12:09:23 -0400

    More comments and description for getch() and ungetch().

--- ch5/getch.c
+++ ch5/getch.c
@@ -8,29 +8,34 @@
  *      Implementation for two simple functions:
  *      getch() - read a character from input into a buffer
  *      ungetch() - push a character back into buffer
+ *      
+ *      getch() fetches from a global buffer (shared by the entire
+ *      application, so be careful) before trying to fetch from
+ *      standartd input with getchar() (standard library function).
  *
  * Build:
  *      $ gcc -c getch.c
  */
 
-#include <stdio.h>
+#include <stdio.h>          /* for getchar() */
 
 #define BUFSIZE 100
 
-char buf[BUFSIZE];      /* buffer for ungetch() */
-int bufp = 0;           /* next free position in buf */
+char buf[BUFSIZE];          /* buffer for ungetch() */
+int bufp = 0;               /* next free position in buf */
 
 /* get a (possibly pushed back) character */
 int getch(void) {
 
+    /* check the global buffer before fetching from standard input */
     return (bufp > 0) ? buf[--bufp] : getchar();
 }
 
+/* put an already feteched character into the global buffer */
 void ungetch(int c) {
 
     if (bufp >= BUFSIZE)
         printf("ungetch: too many characgters\n");
     else
         buf[bufp++] = c;
-    
 }
