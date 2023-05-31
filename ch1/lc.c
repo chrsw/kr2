e7a59ecbd5098b6c9df5f3fe671c328c74a1f40c
Chris Williams <chrsw@users.noreply.github.com>
2023-05-31 10:25:08 -0400

    Add line counting example from secion 1.5.3.

---
+++ ch1/lc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* lc.c: count lines in input */
+
+int main(void) {
+
+    int c,  nl;
+    nl = 0;
+
+    while ( (c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+
+    return 0;
+}
