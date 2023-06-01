9dcbd149f0edff31a5092574516fed42ac20dd17
Chris Williams <chrsw@users.noreply.github.com>
2023-06-01 19:54:15 -0400

    Add limit display helpers.

---
+++ ch3/int_max.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+	printf("%d\n", INT_MAX);
+	return 0;
+}
---
+++ ch3/int_min.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(void) {
+	printf("%d\n", INT_MIN);
+	return 0;
+}
