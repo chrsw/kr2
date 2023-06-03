4371d63fd8259b4e015cb6a792fac13b23f3bdf0
Chris Williams <chrsw@users.noreply.github.com>
2023-06-02 23:43:52 -0400

    Update description for character counter.

--- ch1/nc.c
+++ ch1/nc.c
@@ -7,8 +7,10 @@ int main(void) {
     long nc;
     nc = 0;
 
+    /* read a character from input and count it */
     while (getchar() != EOF)
         ++nc;
+    /* end of file, show all characters counted */
     printf("%ld\n", nc);
 
     return 0;
