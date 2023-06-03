4a5e00d0369303b82a45df67d0eea39b82f07192
Chris Williams <chrsw@users.noreply.github.com>
2023-06-02 23:44:48 -0400

    Add white space trimmer from section 3.7.

---
+++ ch3/trim.c
@@ -0,0 +1,24 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * trim.c
+ *      Remove trailing blanks, tabs, newlines.
+ *
+ * Input:
+ *      Null terminated C string.
+ *
+ */
+
+#include <string.h>
+
+int trim(char s[]);
+
+int trim(char s[]) {
+
+    int n;
+
+    for (n = strlen(s)-1; n >= 0; --n)
+        if (s[n] != ' ' && s[n] != '\t' && s[n] != '\n')
+            break;
+    s[n+1] = '\0';
+    return n;
+}
