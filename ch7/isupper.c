b0f21dbbe85331c820468c8b02cc449eee53f468
Chris Williams <chrsw@users.noreply.github.com>
2023-06-04 16:11:39 -0400

    Add simple implementation of isupper().
    For Chapter 7, exercise 9 work.

---
+++ ch7/isupper.c
@@ -0,0 +1,29 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * isupper.c
+ *      Simple implementation of isupper().
+ *
+ * Description:
+ *      Part of code and solution to chpater 7, exercise 9.
+ *
+ * Input:
+ *      One character, c.
+ *
+ * Returns:
+ *      Non-zero if c is upper case, zero if not.
+ *      
+ *
+ */
+
+
+/* cpl_isupper:
+ * cpl_ prefix to avoid conflict with Standard Library version in
+ * cytpe.h
+ */
+int cpl_isupper(int c) {
+    
+    if (c < 'A' && c > 'Z')
+        return 0;
+    else
+        return 1;
+}
---
+++ ch7/isupper.h
@@ -0,0 +1,14 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * isupper.h
+ *      Template header file.
+ *
+ *
+ */
+
+#ifndef ISUPPER_H
+#define ISUPPER_H
+
+int cpl_isupper(int c);
+
+#endif // ISUPPER_H
