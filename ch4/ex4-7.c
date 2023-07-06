4bad845c3b590697b3b839a4ac3b3e946587dec2
Chris Williams <chrsw@users.noreply.github.com>
2023-07-06 14:00:42 -0400

    Solution 4-7: Push a string back into buffered input.

---
+++ ch4/ex4-7.c
@@ -0,0 +1,40 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex4-7.c
+ *      Write a routine ungets(s) that will push back an entire string
+ *      onto the input.
+ *
+ * Build:
+ *      $ gcc -c ex4-7.c
+ *
+ */
+
+#include <stdio.h>
+
+#define BUFSIZE     100             /* BUFSIZ already defined */
+
+char buf[BUFSIZE];                  /* buffer for ungetch() */
+int bufp = 0;                       /* next free position in buf */
+
+/* get a (possibly pushed back) character */
+int getch(void) {
+
+    return (bufp > 0) ? buf[--bufp]: getchar();
+}
+
+/* push character back on input */
+void ungetch(int c) {
+
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c; 
+}
+
+void ungets(char s[]) {
+
+    if (s != NULL)
+        for (int i; s[i] != '\0'; i++) {
+            ungetch((int)s[i]);
+        }
+}
