04f3ccc1b73e57806f5e227a5081b023502af14e
Chris Williams <chrsw@users.noreply.github.com>
2023-07-08 19:17:48 -0400

    Solution 5-1: Push non-number back into input.

---
+++ ch5/ex5-1.c
@@ -0,0 +1,53 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex5-1.c
+ *      As written in the text, getint() treats a + or - not followed by a
+ *      digit as a valid representation of 0. Fix it to push such a character
+ *      back on the input.
+ *
+ * Description:
+ *      Solution to excercise 5.1.
+ *
+ * Input:
+ *      Standard input.
+ *
+ * Other info:
+ *      Returns, build, notes, etc. 
+ *
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int getch(void);                        /* defined in getch.c */
+void ungetch(int);
+
+int getint(int *pn) {
+   
+    int c, sign;
+ 
+    while (isspace(c = getch())) 
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);                  
+        return 0;                       /* it's not a number */
+    }
+    sign = (c == '-') ? -1 : 1;          /* determine the sign */
+    if (c == '+' || c == '-') {
+        c = getch();
+        if (!isdigit(c)) {
+            ungetch(c);
+            c = *pn;                    /* return orginal value if error */
+            pn = NULL;
+            return c;
+        }
+    }
+    /* convert ascii values into numeric values */
+    for (*pn = 0; isdigit(c); c = getch())
+        *pn = 10 * *pn + (c - '0');
+    *pn *= sign;
+    if (c != EOF)
+        ungetch(c); 
+
+    return c;
+}
