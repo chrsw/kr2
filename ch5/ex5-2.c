d07693c20e70d25fb858a5fe215c9d9ec40ecf98
Chris Williams <chrsw@users.noreply.github.com>
2023-07-09 21:32:05 -0400

    Solution 5-2: minor formatting and cleanup.

--- ch5/ex5-2.c
+++ ch5/ex5-2.c
@@ -3,11 +3,16 @@
  * ex5-2.c
  *      Write getfloat(), the floating point analog of getint().
  *
+ * Description:
+ *      Converts fractional part using snippet from getop() in
+ *      section 4.3.
+ *
  * Input:
- *      Standard input.
+ *      Plain text on standard input.
  *
  * Returns:
- *      Double.
+ *      Double in parameter reference, non-zero for conversion success.
+ *      
  * Build:
  *      $ gcc -c ex5-2.c
  */
@@ -15,7 +20,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int getch(void);                        /* defined in getch.c */
+int getch(void);                                /* defined in getch.c */
 void ungetch(int);
 
 int getfloat(double *pd) {
@@ -23,21 +28,21 @@ int getfloat(double *pd) {
     int c, sign, i;
     double d = 1.0;
 
-    *pd = 0; 
-
     while (isspace(c = getch())) 
         ;
     if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
         ungetch(c);                  
-        return 0;                       /* it's not a number */
+        return 0;                               /* it's not a number */
     }
-    sign = (c == '-') ? -1 : 1;          /* determine the sign */
+
+    sign = (c == '-') ? -1 : 1;                 /* determine the sign */
+
     if (c == '+' || c == '-')
         c = getch();
-    /* collect integer part */
-     for (*pd = 0; isdigit(c); c = getch())      /* conver ascii characters */ 
+                                                /* collect integer part */
+     for (*pd = 0; isdigit(c); c = getch())     /* convert ASCII characters */ 
         *pd = 10 * *pd + (c - '0');             /* to numeric values */
-    if (c == '.')               /* collect fraction part */
+    if (c == '.')                               /* collect fraction part */
         while (isdigit(c=getch())) {
             d /= 10;
             *pd += d * (c - '0');
