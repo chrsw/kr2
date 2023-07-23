42dccac486260c5f08aba0669fe11b82f884062d
Chris Williams <chrsw@users.noreply.github.com>
2023-07-23 18:48:12 -0400

    Minor fixes for strcpy() version 4 in chapter 5.

--- ch5/ch5_strcpy4.c
+++ ch5/ch5_strcpy4.c
@@ -1,7 +1,7 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
- * ch5_strcpy3.c
- *      Copy string from source to destination. 3rd chapter 5 version.
+ * ch5_strcpy4.c
+ *      Copy string from source to destination. 4th chapter 5 version.
  *
  * Description:
  *      Excerpt from chapter 5. This chapter contains a few different
@@ -14,7 +14,7 @@
  *      N/A. Standard library version returns destination string.
  *
  * Build:
- *      $ gcc -Wall -c ch5_strcpy3.c
+ *      $ gcc -Wall -c ch5_strcpy4.c
  *
  */
 
