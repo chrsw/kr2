5f6c8286e267483c8bce8a4b4a8f66eeb1bd67eb
Chris Williams <chrsw@users.noreply.github.com>
2023-07-27 23:26:19 -0400

    Minor comment fixes, chapter 4 atoi() test.

--- ch4/atoi_test.c
+++ ch4/atoi_test.c
@@ -3,9 +3,6 @@
  * atoi_test.c
  *      Test function cpl_atoi().
  *
- * Input:
- *      Describe the expected input.
- *
  * Build:
  *      $ gcc -o atoi_test cpl_atof.c cpl_atoi.c atoi_test.c
  *
@@ -19,7 +16,7 @@ int main(int argc, char *argv[]) {
 
     char s[] = "3.146";
 
-    printf("%d\n", cpl_atoi(s));
+    printf("%d\n", cpl_atoi(s));            /* call the function under test */
 
     return 0;
 }
