87fad82935b12b61a290c83639c3812ece324d3c
Chris Williams <chrsw@users.noreply.github.com>
2023-07-28 20:57:16 -0400

    itoa: Minor comment formatting changes.

--- ch3/itoa.c
+++ ch3/itoa.c
@@ -15,6 +15,7 @@
  * 
  * Build:
  *      $ gcc -o itoa reverse.c itoa.c
+ *
  */
 
 #include <string.h>
@@ -38,8 +39,8 @@ int main(int argc, char *argv[])
 void itoa(int n, char s[]) {
 
     int i, sign;
-    if ((sign = n) < 0)     /* record sign */
-        n = -n;             /* make n positive */
+    if ((sign = n) < 0)         /* record sign */
+        n = -n;                 /* make n positive */
     i = 0;
     do {                        /* generate digits in reverse order */
         s[i++] = n % 10 + '0';  /* get next digit */
