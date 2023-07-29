1272d4d0e9d3aa3d25a66ffd537462ffc5449af1
Chris Williams <chrsw@users.noreply.github.com>
2023-07-28 21:18:13 -0400

    Add chapter 5 version of reverse(); use pointers.

---
+++ ch5/ch5_reverse.c
@@ -0,0 +1,40 @@
+/* vim:ts=4:sw=4:et:
+ *
+ * ch5_reverse.c
+ *      Reverse a string. K&R chapter 5 pointer version.
+ *
+ * To build full app, define REVERSE_MAIN, ex:
+ *      gcc -DREVERSE_MAIN -o reverse ch5_reverse.c
+ */
+
+#include <string.h>
+#include <stdio.h>
+
+//void reverse(char s[]);
+void reverse(char *s);
+
+
+#ifdef REVERSE_MAIN
+int main(void) {
+
+    char s[] = "test me, please";
+    printf("original s: %s\n", s);
+    reverse(s);
+    printf("reversed s: %s\n", s);
+    return 0;
+}
+#endif
+
+
+/* reverse:	reverse string s in place */
+void reverse(char *s) {
+	
+	int c, i, j;
+	
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--) {
+		c = *(s+i);
+		*(s+i) = *(s+j);
+		*(s+j) = c;
+	}
+
+}
