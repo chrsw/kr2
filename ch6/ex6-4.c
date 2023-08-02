08f8fda695a2d2cc50a8d375c89c2cb4fb756b66
Chris Williams <chrsw@users.noreply.github.com>
2023-08-01 21:18:21 -0400

    Solution 6-4: add 1st part of naive solution.
    This solution traverses the unsorted tree of words and only prints if
    the word count matches a certain selection number. The program sweeps
    through a reasonable selection number down to 0 for decreasing sort for
    the test input.
    This version does not ignore common words.

--- ch6/ex6-4.c
+++ ch6/ex6-4.c
@@ -1,23 +1,125 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
  * ex6-4.c
- *     Print words in order of frequency. 
- *
- * Description:
  *      Write a program that prints the distinct words in its input sorted
- *      into decreasing order of frequency of occurance. Precede each word
- *      by its count.
+ *      into decreasing order of frequency of occurrence. Precede each word by
+ *      its count.
  *
  * Input:
- *      List of words on stdin.
+ *      Plain text with words.
+ *
+ * Output:
+ *
+ *
+ * Build:
+ *      $ gcc -o ex6-4 ex6-4.c bgetword.c getch.c
+ *
+ * Run:
+ *      $ ./ex6-4 < test_input.txt
+ *
  */
 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
+struct tnode {                  /* the tree node:  */
+    char *word;                 /* points to the text  */
+    int count;                  /* number of occurrences */
+    struct tnode *left;         /*  left child */
+    struct tnode *right;        /*  right child */
+};
 
-    (void)arcg;                 /* program command line is unused */
-    (void)argv;
+#define MAXWORD 100
+struct tnode *addtree(struct tnode *, char *);
+void treeprint(struct tnode *);
+void sortprint(struct tnode *p);
+void seltreeprint(struct tnode *p, int sel);
+int bgetword(char *, int);
 
-	return 0;
+/* word frequency count */
+int main(int argc, char *argv[]) {
+
+    struct tnode *root;
+    char word[MAXWORD];
+    
+    root = NULL;
+    while (bgetword(word, MAXWORD) != EOF)
+        if (isalpha(word[0]))
+            root = addtree(root, word);
+    sortprint(root);
+    return 0;
+}
+
+struct tnode *talloc(void);
+char *ch6_strdup(char *);
+
+/* addtree: add a node with w, at or below p */
+struct tnode *addtree(struct tnode *p, char *w) {
+
+    int cond;
+    
+    if (p == NULL) {                    /* a new word has arrived */
+        p = talloc();                   /* make a new node */
+        p->word = strdup(w);
+        p->count = 1;
+        p->left = p->right = NULL;
+    } else if ((cond = strcmp(w,p->word)) == 0)
+        p->count++;                     /* repeated word */
+    else if (cond < 0)                  /* less than into left subtree */
+        p->left = addtree(p->left, w);
+    else                                /* greater than into right subtree */
+        p->right = addtree(p->right, w);
+    return p;
+}
+
+/* treeprint: in-order print of tree p */
+void treeprint(struct tnode *p) {
+
+    if (p != NULL) {
+        treeprint(p->left);
+        printf("%4d %s\n", p->count, p->word);
+        treeprint(p->right);
+    }
+}
+
+#include <stdlib.h>
+
+/* talloc: make a tnode */
+struct tnode *talloc(void) {
+
+    return (struct tnode *) malloc(sizeof(struct tnode));
+}
+
+/* strdup: make a duplicate of s */   
+char *ch6_strdup(char *s) {
+
+    char *p;
+    p = (char *) malloc(strlen(s)+1);       /* +1 for '\0' */
+    if (p != NULL)
+        strcpy(p,s);
+    return p;
+}
+
+/* treeprint: in-order selected print of tree p */
+void seltreeprint(struct tnode *p, int sel) {
+
+    if (p != NULL) {
+        seltreeprint(p->left, sel);
+        if (p->count == sel) printf("%4d %s\n", p->count, p->word);
+        seltreeprint(p->right, sel);
+    }
+}
+
+
+void sortprint(struct tnode *p) {
+
+    int i = 20;
+
+    if (p == NULL) return;
+
+    for (i; i > 0; i--) {
+        seltreeprint(p, i);   
+    }
+
 }
