add819ace76de32b607579873ccf9c7138900c65
Chris Williams <chrsw@users.noreply.github.com>
2023-08-19 20:57:17 -0400

    Better descriptions.

--- ch8/syscalls.h
+++ ch8/syscalls.h
@@ -2,11 +2,12 @@
  *
  * syscalls.h
  *      A header file "syscalls.h" is included in many examples in chapter 8.
- *      But the listing for this file is not provided.
- *      It appears this syscalls.h header file is intended to provide the 
+ *      But the listing for this file is not provided. It appears this
+ *      syscalls.h header file is intended to provide the 
  *      declarations for the following system calls (functions) 
  *      The text says this header is "not standard".
  */
+
 #ifndef SYSCALLS_H
 #define SYSCALLS_H 
 
@@ -16,6 +17,7 @@
 #include <unistd.h>
 #include <error.h>
 
+/* Including this header will provide the following functions */
 // read()
 // write()
 // open()
@@ -25,6 +27,7 @@
 // creat() 
 // unlink()
 
+/* These are also defined in chapter 8 version of stdio.h */
 #ifndef BUFSIZ
   #define BUFSIZ 8192
 #endif
