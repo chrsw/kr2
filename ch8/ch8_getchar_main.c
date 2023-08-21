b97e2edc71dbccfacbf18207d8c1441382d94e50
Chris Williams <chrsw@users.noreply.github.com>
2023-08-20 21:06:49 -0400

    Add getchar() main test driver. Explain why it fails.

---
+++ ch8/ch8_getchar_main.c
@@ -0,0 +1,29 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ch8_getchar_main.c
+ *      Test chapter 8 stdio.h
+ *
+ * Build:
+ *      $ gcc -o ch8_getchar_main fillbuf.c ch8_getchar_main.c
+ *
+ * Notes:
+ *      After running this code in a GDB session, the line:
+ *      if ((fp-flag&(_READ|_EOF_)) != _READ)
+ *      always returns true. getchar() calls getc(stdin) which means
+ *      stdin, &_iob[0] is being passed to getc(). _iob[] is defined in
+ *      this file, but does it do anything? It's not initialized to 
+ *      anything.
+ *      The C Standard Library runtime is supposed to set up _iob[]'s so 
+ *      they are linked to the actual files, but that isn't happening here.
+ */
+
+#include "stdio.h"
+
+FILE _iob[OPEN_MAX];
+
+int main(void) {
+    
+    int c = 0; 
+    c = getchar();
+    return c;
+}
