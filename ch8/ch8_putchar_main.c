6b5579b3eefb5e1cfc9277ea7e8c5d7fc033e9e3
Chris Williams <chrsw@users.noreply.github.com>
2023-08-25 22:56:32 -0400

    Solution 8-3: Test putc() on buffered output.
    Use shorter buffer size for test.
    Update build instructions in header.

--- ch8/ch8_putchar_main.c
+++ ch8/ch8_putchar_main.c
@@ -4,21 +4,23 @@
  *      Test custom stdio.h
  *
  * Build:
- *      $ gcc -o ch8_putchar_main ch8_putchar_main.c
+ *      $ gcc -o ch8_putchar_main flushbuf.c ch8_putchar_main.c
  */
 
 #include "stdio.h"
 
 FILE _iob[OPEN_MAX] = {         /* stdin, stdout, stderr */
     {0, (char *)0, (char *)0, _READ, 0 },
-    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 1 },
+    {0, (char *)0, (char *)0, _WRITE, 1 },
     {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 }
 };
 
 
 int main(void) {
     
-    char c = 0x61;
-    putchar(c);
+    int i = 0;
+
+    for (i = 0x62; i < 0x7B; i++)
+        putchar((char)i);
     return 0;
 }
