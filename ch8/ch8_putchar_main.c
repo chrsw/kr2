84a74f0c199253134e4f5c55f2e8aeabb165c4fd
Chris Williams <chrsw@users.noreply.github.com>
2023-08-25 22:42:48 -0400

    Solution 8-3: Unbuffered stdout, temporarily.

--- ch8/ch8_putchar_main.c
+++ ch8/ch8_putchar_main.c
@@ -9,7 +9,12 @@
 
 #include "stdio.h"
 
-FILE _iob[OPEN_MAX];
+FILE _iob[OPEN_MAX] = {         /* stdin, stdout, stderr */
+    {0, (char *)0, (char *)0, _READ, 0 },
+    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 1 },
+    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 }
+};
+
 
 int main(void) {
     
