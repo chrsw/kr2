97e0c75733d7fbb8b1ab8a4472bf99b666f390a8
Chris Williams <chrsw@users.noreply.github.com>
2023-08-26 19:59:30 -0400

    Solution 8-3: Add fflush() test driver.

---
+++ ch8/ch8_flush_main.c
@@ -0,0 +1,29 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ch8_flush_main.c
+ *      Test fflush() from Exercise 8-3.
+ *
+ * Build:
+ *      $ gcc -o ch8_flush_main fflush.c flushbuf.c ch8_flush_main.c
+ */
+
+#include "stdio.h"
+
+int fflush(FILE *);
+
+FILE _iob[OPEN_MAX] = {       
+    {0, (char *)0, (char *)0, _READ, 0 },               /* stdin */
+    {0, (char *)0, (char *)0, _WRITE, 1 },              /* stdout */
+    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 }      /* stderr */
+};
+
+int main(void) {
+    
+    int i;
+           /* 'a' */  /* 'z' */
+    for (i = 0x61; i <= 0x7A; i++)
+        putchar((char)i);
+    putchar('\n');
+    fflush(stdout);
+    return 0;
+}
