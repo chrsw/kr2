f9c93770bb2da84f9f1f9175a987aa7d29427137
Chris Williams <chrsw@users.noreply.github.com>
2023-08-26 11:00:22 -0400

    Minor doc improvements of _flushbuf() test driver.

--- ch8/ch8_putchar_main.c
+++ ch8/ch8_putchar_main.c
@@ -1,7 +1,7 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
  * ch8_putchar_main.c
- *      Test custom stdio.h
+ *      Test putchar(), putc() and _fillbuf() in custom stdio.h
  *
  * Build:
  *      $ gcc -o ch8_putchar_main flushbuf.c ch8_putchar_main.c
@@ -9,18 +9,17 @@
 
 #include "stdio.h"
 
-FILE _iob[OPEN_MAX] = {         /* stdin, stdout, stderr */
-    {0, (char *)0, (char *)0, _READ, 0 },
-    {0, (char *)0, (char *)0, _WRITE, 1 },
-    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 }
+FILE _iob[OPEN_MAX] = {       
+    {0, (char *)0, (char *)0, _READ, 0 },               /* stdin */
+    {0, (char *)0, (char *)0, _WRITE, 1 },              /* stdout */
+    {0, (char *)0, (char *)0, _WRITE | _UNBUF, 2 }      /* stderr */
 };
-
 
 int main(void) {
     
-    int i = 0;
-
-    for (i = 0x62; i < 0x7B; i++)
+    int i;
+           /* 'a' */  /* 'z' */
+    for (i = 0x61; i <= 0x7B; i++)
         putchar((char)i);
     return 0;
 }
