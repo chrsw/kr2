89b8c655c42c9f7a83defc1a3882fb04b648ffc2
Chris Williams <chrsw@users.noreply.github.com>
2023-08-27 13:41:41 -0400

    Solution 8-3: Dynamic input data length calculator.

--- ch8/ch8_fclose_main.c
+++ ch8/ch8_fclose_main.c
@@ -1,13 +1,34 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
  * ch8_fclose_main.c
- *      Test fclose() from Exercise 8-3.
+ *      Test fclose() and fflush() from Exercise 8-3.
+ *      
+ *      An input file with a hard coded path is opened for reading. An output
+ *      file also with a hard coded path is opened for writing. the length
+ *      of the input file is larger than the length of a file's buffer.
+ *      
+ *      The entire input file is read into a statically allocated buffer.
+ *      The application buffer is written to the file's buffer, the file's
+ *      buffer is eventually filled up then more data is written to the file
+ *      buffer.
+ *
+ *      When then file is closed with fclose(), the remaining data in the
+ *      file's buffer is written out to the file with fflush(), called from
+ *      within fclose().
+ *
+ *      No data is written to standard out in this version. Input file must
+ *      be smaller than the length of the statically allocated buffer.
  *
  * Build:
- *      $ gcc -o ch8_fclose_main ch8_fopen.c fclose.c flushbuf.c \
+ *      $ gcc -o ch8_fclose_main ch8_fopen.c fclose.c fflush.c flushbuf.c \
  *      ch8_fillbuf.c ch8_fclose_main.c
+ *
+ * TODO:
+ *      Implement better error handling.
+ *
  */
 
+#include <string.h>
 #include "stdio.h"
 
 int fflush(FILE *);
@@ -28,22 +49,24 @@ int main(void) {
     FILE *r;
     FILE *w;
     char c;
+    int len;
 
     r = ch8_fopen("ch8_fclose_test.txt", "r");
     w = ch8_fopen("ch8_fclose_write_test.txt", "w");
-    if (r == NULL)
-        return 1;
-    if (w == NULL)
+    if (r == NULL || w == NULL )
         return 1;
 
     while ((c = getc(r)) != EOF)
         buf[i++] = c;
     buf[i] = '\0';
+    len = strlen(buf);
+
     i = 0;
-    while (i < 30)
+    while (i < len)
         putc(buf[i++], w);
 
     fclose(r);
     fclose(w);
+
     return 0;
 }
