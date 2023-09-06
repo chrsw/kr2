7a7e4e763f579052190910f16eaebc7b7f8ca8e5
Chris Williams <chrsw@users.noreply.github.com>
2023-09-06 18:58:53 -0400

    Solution 5-13: Add start of solution, tail program.

---
+++ ch5/tail.c
@@ -0,0 +1,68 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * tail.c
+ *      Print the last 10 or n lines of input. Exercise 5-13.
+ *
+ * Description:
+ *      Problem description from text. Chapater 5, exercise 13:
+ *      Write the program `tail` which prints the last n lines of its input.
+ *      By default, n is set to 10, but it can be changed by an optional
+ *      argument so that
+ *
+ *      $ tail -n
+ *
+ *      prints the last n lines.
+ *
+ * Input:
+ *      Plain text.
+ *
+ * Output:
+ *      Last 10 or n lines of input.
+ *
+ * Build:
+ *
+ * Run:
+ *      An example of how this program should be run.
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int ex5_6_getline(char *s, int lim);
+
+const int limit = 100;
+
+int main(int argc, char *argv[]) {
+
+    char s[10][limit];
+    char line[limit];
+    int lc = 0;
+    int i = 0;
+
+    // Read in every line
+    // add the new recent line to the s array
+    // or
+    // push all other lines down s array
+    // then add the new line to the end of the array
+    while (ex5_6_getline(line, limit) > 0) {
+        
+        if (lc < 10) {
+            strcpy(s[lc++], line);
+        } else {
+            for (i = 0; i < 10; i++) {
+                strcpy(s[i],s[i+1]);
+            }
+            strcpy(s[lc], line);
+        }
+        
+    }
+
+    for (i = 0; i <= 9; i++)
+        printf("%s", s[i]);
+
+    return 0;
+}
