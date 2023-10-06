25180120f76fff6205bea71a75c5edabbbcaaa13
Chris Williams <chrsw@users.noreply.github.com>
2023-10-05 20:44:41 -0400

    Add printd() from Section 4.10.

--- ch4/printd.c
+++ ch4/printd.c
@@ -1,25 +1,21 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
  * printd.c
- *      Print a decimal integer recursively.
- *
- * Description:
- *      Excerpt from Section 4.10, Recursion
+ *      Print n in decimal - recursive.
+ *      Excerpt from Section 4.10 Recursion.
  *
  */
 
 #include <stdio.h>
 
-/* print n in decimal */
-void printd(int n) {
-
+/* printd: print n in decimal */
+void printd(int n)
+{
     if (n < 0) {
         putchar('-');
         n = -n;
     }
-    
-    if (n / 10) 
-        printd(n/10);
-    putchar(n % 10 + '0');     
-    
+    if (n /10)
+        printd(n / 10);
+    putchar(n % 10 + '0');
 }
