193d4eb20746e7de81e7282e8022b9ee70e87ecf
Chris Williams <chrsw@users.noreply.github.com>
2023-10-06 23:01:43 -0400

    Solution 4-12: Add file comment header from template.

--- ch4/ex4-12_main.c
+++ ch4/ex4-12_main.c
@@ -1,3 +1,17 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ex4-12_main.c
+ *      Main driver for recursive itoa(). Part of exercise 4-12 solution.
+ *      Details on how the code you're reading implements the design.      
+ *
+ * Build:
+ *      $ gcc -o itoa ex4-12_main.c itoa.c
+ *
+ * Run:
+ *      $ ./itoa
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
@@ -17,5 +31,6 @@ int main(void) {
 		return -1;
 	itoa(n, s);	
 	printf("%s\n", s);
+
 	return 0;
 }
