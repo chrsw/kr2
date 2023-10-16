9cbdd6551331283fd35c612852b3f3b657dba76c
Chris Williams <chrsw@users.noreply.github.com>
2023-10-15 22:14:45 -0400

    Fix infinite bug in chapter 5 strcmp().

--- ch5/strcmp.c
+++ ch5/strcmp.c
@@ -4,55 +4,40 @@
  *      Compare strings s, t. Return -1 if s less than t, 0 if s equal to t
  *      and 1 if s greater than t.
  *
- * Description:
- *      Template for code and solutions to exercises in The C Programming
- *      Language 2nd Ed. A longer description of the program goes here.
- *
  * Input:
  *      Describe the expected input.
  *
  * Build:
  *      $ gcc -c strcmp.c
  *
- *
  * Run:
  *      An example of how this program should be run.
  *
- * Nots:
- *      Helpful information for anyone to have who is maintaining this code.
- *
  */
 
-#include <stdio.h>
+
+int ch5_strcmp(char *s, char *t);
 
 int ch5_strcmp(char *s, char *t) {
 
     int ret = 0;
-    int len_s, len_t;
-    len_s = 0;
-    len_t = 0; 
     char *s2 = s;
     char *t2 = t;
 
-    while (*s2++) ++len_s;
-    while (*t2++) ++len_t;
     
-    if (len_s > len_t)
-        return 1;
-    else if (len_s < len_t)
-        return -1;
-    else
-        ;
-  
-    /* strings are same length */ 
     while (*s != '\0') {
         if (*s > *t)
-            ret = 1;
+            return 1;
         else if (*s < *t)
-            ret = -1;
-        else
-            ;
+            return -1;
+        else {
+            s++;
+            t++;
+        }
     }
-        
+
+    if (strlen(t2) > strlen(s2))
+       return -1; 
+
     return ret;
 }
