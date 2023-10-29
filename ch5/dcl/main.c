89656a170fad24688069d0f869912382ad176f2f
Chris Williams <chrsw@users.noreply.github.com>
2023-10-29 19:01:53 -0400

    dcl: minor comment, format and error report.

--- ch5/dcl/main.c
+++ ch5/dcl/main.c
@@ -43,7 +43,7 @@ char name[MAXTOKEN];		/* identifier name */
 char datatype[MAXTOKEN];	/* data type = char, int, etc. */
 char out [1000];		/* output string */
 
-
+/* convert declaration to words */
 int main(int argc, char *argv[]) {
 
     (void)argc;
@@ -54,7 +54,7 @@ int main(int argc, char *argv[]) {
         out[0] = '\0';
         dcl();                      /* parse rest of line */
         if (tokentype != '\n')
-            printf("syntax error\n");
+            printf("dcl: syntax error\n");
         printf("%s: %s %s\n", name, out, datatype);
     }
     return 0;
