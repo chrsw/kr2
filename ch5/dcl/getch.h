691f680ded0f275ab1c545301ed6a8061890c754
Chris Williams <chrsw@users.noreply.github.com>
2023-10-29 17:34:39 -0400

    dcl: add header for getch().

---
+++ ch5/dcl/getch.h
@@ -0,0 +1,14 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * template.h
+ *      Template header file.
+ *
+ *
+ */
+#ifndef GETCH_H 
+#define GETCH_H
+
+int getch(void);
+void ungetch(int);
+
+#endif	/* GETCH_H */
