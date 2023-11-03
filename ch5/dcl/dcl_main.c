fb1c1f603f1269317aaa262706e5eb73273753c2
Chris Williams <chrsw@users.noreply.github.com>
2023-11-02 23:53:25 -0400

    dcl: update comments in main file.

--- ch5/dcl/dcl_main.c
+++ ch5/dcl/dcl_main.c
@@ -1,34 +1,25 @@
 /* vim:ts=4:sw=4:et:so=10:ls=2:
  *
- * dcl.c
+ * dcl_main.c
  *      dcl app main().
  *
- * Description:
- *      Template for code and solutions to exercises in The C Programming
- *      Language 2nd Ed. A longer description of the program goes here.
- *
  * Input:
- *      Describe the expected input.
+ *      C declaration.
+ *
+ * Description:
+ *      Excerpt for text section 5.12.
  *
  * Output:
- *      What output does this program generate? stdout, stderr, files, etc.
- *
- * Design:
- *      Details about the design, theory and options taken for the
- *      implemented solution.
- * 
- * Implementation:
- *      Details on how the code you're reading implements the design.      
+ *      Plain text English description of the C declaration on the input.
  *
  * Build:
- *      $ gcc -Wall -Wextra -Wpedantic -o dcl main.c dcl.c gettoken.c getch.c
+ *      $ gcc -Wall -Wextra -Wpedantic -o dcl dcl_main.c dcl.c gettoken.c\
+ *      getch.c
  *
  * Run:
  *      ./dcl # Then type input line, hit enter, hit Ctrl-D
- *
- * Notes:
- *      Seems to work better when input is typed into stdin rather then
- *      redirected from file.
+ *      - or -
+ *      ./dcl < dcl_test.txt
  *
  */
 
@@ -40,9 +31,9 @@
 
 int tokentype;			/* type of the last token */
 char token[MAXTOKEN];		/* last token string */
-char name[MAXTOKEN];		/* identifier name */
-char datatype[MAXTOKEN];	/* data type = char, int, etc. */
-char out [1000];		/* output string */
+char name[MAXTOKEN];           /* identifier name */
+char datatype[MAXTOKEN];       /* data type = char, int, etc. */
+char out [1000];               /* output string */
 
 /* convert declaration to words */
 int main(int argc, char *argv[]) {
