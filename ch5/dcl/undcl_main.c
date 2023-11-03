08b8773821a4032b767037243c65e4f64a907cef
Chris Williams <chrsw@users.noreply.github.com>
2023-11-02 23:53:53 -0400

    dcl: add rest of undcl and test input file.
    The PDF copy of the book this snippet originally came from was missing
    the line that prints the final output. The hardcopy text has this line.

--- ch5/dcl/undcl_main.c
+++ ch5/dcl/undcl_main.c
@@ -4,7 +4,8 @@
  *      Convert word descriptions to declaration.
  *
  * Build:
- *      $ gcc -o dcl dcl_main.c
+ *      $ gcc -Wall -Wextra -Wpedantic -o undcl undcl_main.c dcl.c gettoken.c\
+ *      getch.c
  *
  * Run:
  *      $ ./dcl < dcl_test.txt
@@ -12,13 +13,25 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "gettoken.h"
+#include "dcl.h"
+
+int type;
+char temp[2000];
+int tokentype;
+char name[MAXTOKEN];           /* identifier name */
+char token[MAXTOKEN];           /* identifier name */
+char datatype[MAXTOKEN];       /* data type = char, int, etc. */
+char out [1000];               /* output string */
+
 
 /* undcl:  convert word descriptions to declaration */
 int main(int argc, char *argv[]) {
 
-    int type;
-    char temp[MAXTOKEN];
-    
+    out[0] = '\0';
+
     while (gettoken() != EOF) {
         strcpy(out, token);
         while ((type = gettoken()) != '\n')
@@ -33,5 +46,7 @@ int main(int argc, char *argv[]) {
             } else 
                 printf("invalid input at %s\n", token);
     }
+    
+    printf("%s\n", out);
     return 0;
 }
