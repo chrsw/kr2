7261148ebcacc49e612d75429f5bfa634b786ac7
Chris Williams <chrsw@users.noreply.github.com>
2023-11-02 23:23:17 -0400

    dcl: Add comments to undcl.

--- ch5/dcl/undcl_main.c
+++ ch5/dcl/undcl_main.c
@@ -1,34 +1,13 @@
 /* vim:ts=4:sw=4:et:so=10:ls=2:
- *      Vim modeline for consistent editor settings across files.
  *
- * undcl.c
- *      A brief description of the file.
- *
- * Description:
- *      Template for code and solutions to exercises in The C Programming
- *      Language 2nd Ed. A longer description of the program goes here.
- *
- * Input:
- *      Describe the expected input.
- *
- * Output:
- *      What output does this program generate? stdout, stderr, files, etc.
- *
- * Design:
- *      Details about the design, theory and options taken for the
- *      implemented solution.
- * 
- * Implementation:
- *      Details on how the code you're reading implements the design.      
+ * undcl_main.c
+ *      Convert word descriptions to declaration.
  *
  * Build:
- *      How to build this program or a build example (incl make targets).
+ *      $ gcc -o dcl dcl_main.c
  *
  * Run:
- *      An example of how this program should be run.
- *
- * Notes:
- *      Helpful information for anyone to have who is maintaining this code.
+ *      $ ./dcl < dcl_test.txt
  *
  */
 
