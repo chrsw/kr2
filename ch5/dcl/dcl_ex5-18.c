7ad22136ae856ef2b39bb1aa9b87e9d561682616
Chris Williams <chrsw@users.noreply.github.com>
2023-11-17 21:31:51 -0500

    Exercise 5-18: dcl: use int for missing type.
    A naive addition to the dcl application where a default type of 'int' is
    used instead of throwing an error for missing type. The text says the
    dcl example is for explanatory purposes and so are these solutions;
    they're not robust against deeper errors.
    Start moving test code to a test sub-directory.

--- ch5/dcl/dcl_ex5-18.c
+++ ch5/dcl/dcl_ex5-18.c
@@ -7,19 +7,22 @@
  *      Make dcl recover from input errors.
  *
  * Input:
- *      C declaration.
+ *      C declaration. Declarations are a subset of legal C declarations and
+ *      must only span 1 line.
  *
  * Output:
  *      Plain text English description of the C declaration on the input.
  *
  * Build:
- *      $ gcc -Wall -Wextra -Wpedantic -o dcl dcl_main.c dcl.c gettoken.c\
+ *      $ gcc -Wall -Wextra -Wpedantic -o dcl-ex5-18 dcl_ex5-18.c dcl.c gettoken.c\
  *      getch.c
  *
  * Run:
- *      ./dcl # Then type input line, hit enter, hit Ctrl-D
+ *      $ ./dcl # Then type input line, hit enter, hit Ctrl-D
  *      - or -
- *      ./dcl < dcl_test.txt
+ *      $ ./dcl < dcl_test.txt # to redirect from a file
+ *      - or -
+ *      $ echo "int *x" | ./dcl-5-18
  *
  */
 
@@ -28,27 +31,36 @@
 #include <ctype.h>
 #include "dcl.h"
 #include "gettoken.h"
+#include "getch.h"
 
 
-int tokentype;			/* type of the last token */
-char token[MAXTOKEN];		/* last token string */
-char name[MAXTOKEN];           /* identifier name */
-char datatype[MAXTOKEN];       /* data type = char, int, etc. */
-char out [1000];               /* output string */
-
+int tokentype;			        /* type of the last token */
+char token[MAXTOKEN];		    /* last token string */
+char name[MAXTOKEN];            /* identifier name */
+char datatype[MAXTOKEN];        /* data type = char, int, etc. */
+char out [1000];                /* output string */
+int first = 0;                  /* first time through gettoken */
 int is_datatype(char *s);
 
 /* convert declaration to words */
 int main(int argc, char *argv[]) {
 
+    /* silence -Wunused-parameter */
     (void)argc;
     (void)argv;
 
+    datatype[0] = '\0';
+    strcpy(datatype, "int");   /* integer by default */
     while (gettoken() != EOF) {     /* first token on line */
-        if (is_datatype(token))
-            strcpy(datatype, token);     /* is the datatype */
-        else
-            strcpy(datatype, "int");
+        if (is_datatype(token)) {    /* is the datatype */
+            printf("datatype is %s\n", token);
+            strcpy(datatype, token);
+        } else {
+            printf("missing datatype: token = %s\n", token);
+            printf("                  name = %s\n", name);
+            printf("                  tokentype = 0x%02X\n", tokentype);
+            ungetch(tokentype);
+        }
         out[0] = '\0';
         dcl();                      /* parse rest of line */
         if (tokentype != '\n')
@@ -60,7 +72,23 @@ int main(int argc, char *argv[]) {
 
 
 /* is_datatype:  check to see if a token is a valid datatype */
-int is_datatype(char *s)
-{
-   return (!strcmp(s, "void"));
+int is_datatype(char *s) {
+    
+    int ret = 0;
+    int idx = 0;
+    char *types[] = {"char", "double", "float", 
+                     "int", "long", "short", 
+                     "unsigned", "void", NULL};
+
+    while (types[idx] != NULL) {
+        if (strcmp(types[idx], s) == 0) {
+            ret = 1;
+            break;
+        }
+        idx++;
+    }
+    return ret;
 }
+
+
+
