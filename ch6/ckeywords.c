e667030f753234e5f1ab4d204e34706c3a7573b4
Chris Williams <chrsw@users.noreply.github.com>
2023-12-19 15:55:37 -0500

    C keywords: clean warnings and formatting.

--- ch6/ckeywords.c
+++ ch6/ckeywords.c
@@ -10,7 +10,8 @@
  *      A valid C file.
  *
  * Build:
- *     $ gcc -o ckeywords ckeywords.c getword.c getch.c binsearch.c
+ *     $ gcc -Wall -Wextra -Wpedantic -o ckeywords ckeywords.c getword.c getch.c
+ *       binsearch.c
  *
  * Run:
  *      ./ckeywords < file.c
@@ -31,42 +32,42 @@ int binsearch(char *, struct key *, int);
 /* keyword list from Section A2.4 Keywords, page 192 */
 struct key keytab[] = {
 
-    "auto", 0,
-    "break", 0,
-    "case", 0,
-    "char", 0,
-    "const", 0,
-    "continue", 0,
-    "default", 0,
-    "do", 0,
-    "double", 0,
-    "else", 0,
-    "enum", 0,
-    "extern", 0,
-    "float", 0,
-    "for", 0,
-    "goto", 0,
-    "if", 0,
-    "int", 0,
-    "long", 0,
-    "register", 0,
-    "return", 0,
-    "short", 0,
-    "signed", 0,
-    "sizeof", 0,
-    "static", 0,
-    "struct", 0,
-    "switch", 0,
-    "typedef", 0,
-    "union", 0,
-    "unsigned", 0,
-    "void", 0,
-    "volatile", 0,
-    "while", 0
+    {"auto",        0},
+    {"break",       0},
+    {"case",        0},
+    {"char",        0},
+    {"const",       0},
+    {"continue",    0},
+    {"default",     0},
+    {"do",          0},
+    {"double",      0},
+    {"else",        0},
+    {"enum",        0},
+    {"extern",      0},
+    {"float",       0},
+    {"for",         0},
+    {"goto",        0},
+    {"if",          0},
+    {"int",         0},
+    {"long",        0},
+    {"register",    0},
+    {"return",      0},
+    {"short",       0},
+    {"signed",      0},
+    {"sizeof",      0},
+    {"static",      0},
+    {"struct",      0},
+    {"switch",      0},
+    {"typedef",     0},
+    {"union",       0},
+    {"unsigned",    0},
+    {"void",        0},
+    {"volatile",    0},
+    {"while",       0}
 };
 
 /* Get size of list with a programmatic solution */ 
-unsigned int listsize = (unsigned int)(sizeof(keytab)/sizeof(struct key));
+int listsize = (unsigned int)(sizeof(keytab)/sizeof(struct key));
 
 int main(int argc, char *argv[])
 {
@@ -76,13 +77,15 @@ int main(int argc, char *argv[])
     int n;
     char word[MAXWORD];
 
-    while (bgetword(word, MAXWORD) != EOF)   /* count keywords */
+    while (bgetword(word, MAXWORD) != EOF) {   /* count keywords */
         if (isalpha(word[0]))
             if ((n = binsearch(word, keytab, listsize)) >= 0)
                 keytab[n].count++;
-    for (n = 0; n < listsize; n++)             /* print keyword counts */
+    }
+    for (n = 0; n < listsize; n++) {            /* print keyword counts */
         if (keytab[n].count > 0)
             printf("%4d %s\n", keytab[n].count, keytab[n].word);
+    }
 	return 0;
 }
 
