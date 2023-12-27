1b0da1da63ab149110ff28814e4fd57a44f58957
Chris Williams <chrsw@users.noreply.github.com>
2023-12-26 21:36:05 -0500

    Solution 6-1: Add underscore handling to getword().
    This commit is a cleaner version of the solution to the first part of
    exercise 6-1, add underscore support to getword(). This is a very short
    change for bare minimum functionality. This solution will also include
    test inputs and test outputs in follow-on commits.
    This commit also has a much more expanded description of the original
    getword() function in the commend header for ex6_1_getword().

--- ch6/ex6-1.c
+++ ch6/ex6-1.c
@@ -6,7 +6,18 @@
  * Description:
  *      Get words from input. 
  *      Handle string literals.
- *      Handle preprocessor control lines
+ *      Handle preprocessor control lines.
+ *
+ * Input:
+ *      char *w:    String to store word, if found.
+ *      int lim:    Max word length.
+ *
+ * Output:
+ *      Returns first character of word found.
+ *      Copies found word into character string passed as 1st parameter.
+ *
+ * Design:
+ *      TBD
  *
  * Build:
  *      $ gcc -Wall -c ex6-1.c
@@ -39,8 +50,7 @@ static bool newline = false;
 /* getword: get next word or character from input 
  *          very similar version in K&R2, page 136 
  */
-int getword(char *word, int lim)
-{
+int getword(char *word, int lim) {
 
     int c;
     char *w = word;
@@ -74,6 +84,7 @@ int getword(char *word, int lim)
  *              word[0] non-alphanumeric if word is skipped.
  *
  *              Preprocessor lines start at the beginning of a line
+ *
  * TODO:        Find out if we're in a comment or not.
  */
 int bgetword(char *word, int lim)
@@ -81,7 +92,7 @@ int bgetword(char *word, int lim)
     int c;
                                             /* Don't count words in */
                                             /* comments */
-    bool cmt[3] = {false, false, false};  /* track comment state */ 
+    bool cmt[3] = {false, false, false};    /* track comment state */ 
     char *w = word;
 
     comstate comment_state = NONE;
@@ -161,6 +172,77 @@ int getwordb(char *word, int lim)
         if (!isalnum(*w = getchb())) {
         ungetch(*w);
         break;
+    }
+    *w = '\0';
+    return word[0];
+
+}
+
+
+/* ex6_1_getword:  getword with added capabilities from Exercise 6.1
+ *
+ * Original description:
+ *
+ *      This function gets it's input from buffered standard input via
+ *      getch().
+ *      
+ *      This function is called in a while loop looking for words or EOF.
+ *
+ *      Skips the leading white space characters (isspace returns true if
+ *      character in argument is form-feed, newline, carraige return,
+ *      horizontal tab or vertical tab).
+ *
+ *      Checks that the first non-whitespace character is not EOF. Function
+ *      will return if the first non-whitespace character is EOF, setting the
+ *      first character of word in the parameter to NULL then returning EOF.
+ *
+ *      If the first character found is not an upper case letter or a lower
+ *      case letter (isalpha() == islower() || isupper() then the function
+ *      also sets the 1st charcter of the input word string to
+ *      NULL then immediately returns the non-letter character.
+ *
+ *      Once the EOF and non-letter checks are passed, then we assume we are
+ *      at the start of a word. This function then calls getch() repeatedly
+ *      until a non-alphanumber character is found (isalnum() == 
+ *      isalpha() || isdigit()).
+ *
+ *      In the word building loop, the found alphanumeric characters are added
+ *      to the string in the 1st parameter.
+ *
+ *      When the first non-letter or non-number character is found in the
+ *      word building loop, the loop pushes the non-letter or non-number
+ *      character back into the input buffer (it will be the next char read)
+ *      and the loops ends.
+ *
+ *      After the word-building loops ends (other failed checks would have
+ *      ended the function even earlier) then the function puts a NULL
+ *      character at the end of the word string in the parameter list and
+ *      returns the first letter of the found word.
+ *
+ *      When a first word character is a non-letter, it is not pushed back
+ *      into the input buffer. Instead the caller is given a single character
+ *      word (NULL) in the word string in the parameter list and given
+ *      the non-letter character in the integer returned by the function.
+ *
+ * */
+int ex6_1_getword(char *word, int lim)
+{
+
+    int c;
+    char *w = word;
+
+    while (isspace(c = getch())) /* find the first non-white space */
+        ;
+    if (c != EOF)               /* still haven't reached EOF... */
+        *w++ = c;
+    if (!isalpha(c)) {
+        *w = '\0';
+        return c;
+    }
+    for ( ; --lim > 0; w++)
+        if (!(isalnum(*w = getch())) && (*w != '_')) {
+            ungetch(*w);
+            break;
     }
     *w = '\0';
     return word[0];
