23279fd5204e753cfc9a1598e77009667a6de3d7
Chris Williams <chrsw@users.noreply.github.com>
2023-12-29 22:56:17 -0500

    cow: Fix tree.h include guard

--- ch6/cow/tree.h
+++ ch6/cow/tree.h
@@ -1,15 +1,15 @@
 /* vim:ts=4:sw=4:et:so=10:
  *
- * template.h
+ * tree.h
  *      Template header file.
  *
  *
  */
 
-#ifndef TEMPLATE_H
-#define TEMPLATE_H
+#ifndef TREE_H
+#define TREE_H
 
 void treeprint(struct tnode *);
 struct tnode *addtree(struct tnode *p, char *);
 
-#endif /* TEMPLATE_H */
+#endif /* TREE_H */
