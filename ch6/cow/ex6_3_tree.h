4dc7a2348e51d83cf3cfa0fd4cd7bb73014dc634
Chris Williams <chrsw@users.noreply.github.com>
2024-01-01 19:38:59 -0500

    Minor formatting clean-up.

--- ch6/cow/ex6_3_tree.h
+++ ch6/cow/ex6_3_tree.h
@@ -2,13 +2,12 @@
  *
  * ex6_3_tree.h
  *
- *
  */
 
 #ifndef EX6_3_TREE_H
 #define EX6_3_TREE_H
 
 void treeprint(struct tnode *);
-struct tnode *addtree(struct tnode **pp, char *);
+struct tnode *addtree(struct tnode **, char *);
 
 #endif /* EX6_3_TREE_H */
