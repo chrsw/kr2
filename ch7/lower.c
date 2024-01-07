e67e0ea0a8216016c85b18877c71eeb7ff745961
Chris Williams <chrsw@users.noreply.github.com>
2024-01-06 21:43:25 -0500

    Add lower from section 7.1.

---
+++ ch7/lower.c
@@ -0,0 +1,30 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * lower.c
+ *      Covert input to lower case.
+ *
+ * Description:
+ *      K&R2 section 7.1 excerpt.
+ *
+ * Build:
+ *      $ gcc -o lower lower.c
+ *
+ * Run:
+ *      $ ./lower < test_input_lower.txt
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+
+/* lower:  convert input to lower case. */
+int main(int argc, char *argv[])
+{
+    int c;
+
+    while ((c = getchar()) != EOF)
+        putchar(tolower(c));
+    return 0;
+}
