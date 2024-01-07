4336e26407767f1ea9887d2f77c509dfa19d1613
Chris Williams <chrsw@users.noreply.github.com>
2024-01-06 22:13:34 -0500

    Solution 7-2: Add solution and test input.

---
+++ ch7/sensible.c
@@ -0,0 +1,53 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * sensible.c
+ *      Print arbitrary input in a sensible way.
+ *
+ * Description:
+ *      Solution to Chapter 7, exercise 2.
+ *      Exercise text: Write a program that will print arbitrary input in a
+ *      sensible way. As a minimum, it should print non-graphic characters
+ *      in octal or hexidecimal according to local custom, and break long
+ *      text lines.
+ *
+ * Build:
+ *      $ gcc -o sensible sensible.c
+ *
+ * Run:
+ *      $ ./sensible < ex7_2_test_input.txt
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    int c, cc;
+    int len = 0;
+    while((c = getchar()) != EOF) {
+         
+        if (isprint(c))             /* print a char if it's readable */
+            putchar(c);
+        else if (isspace(c)) {
+            putchar(c);
+            if (c == '\n')
+                len = 0;
+        }
+        else
+            printf("0%o", c);       /* otherwise print the octal value */
+
+        if (len++ > 80) {           /* break long lines */ 
+
+            printf("\n");
+            len = 0;
+        }
+        cc = c;
+    }
+    
+    if (cc != '\n') printf("\n");   /* end on one new line */ 
+    return 0;
+}
