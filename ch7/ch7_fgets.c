4af5fc9d7544482bbbfd2cb8799d8c95b43b6075
Chris Williams <chrsw@users.noreply.github.com>
2024-01-07 19:28:21 -0500

    Add chapter 7 version of fgets().

---
+++ ch7/ch7_fgets.c
@@ -0,0 +1,50 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * ch7_fgets.c
+ *      Get at most n chars from file.
+ *
+ * Description:
+ *      Excerpt from text.
+ *      Part of solution to exercise 7-8.
+ *
+ * Input:
+ *      Describe the expected input.
+ *
+ * Output:
+ *      What output does this program generate? stdout, stderr, files, etc.
+ *
+ * Design:
+ *      Details about the design, theory and options taken for the
+ *      implemented solution.
+ * 
+ * Implementation:
+ *      Details on how the code you're reading implements the design.
+ *
+ * Build:
+ *      How to build this program or a build example (incl make targets).
+ *
+ * Run:
+ *      An example of how this program should be run.
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include <stdio.h>
+#include "ch7_fgets.h"
+
+char * ch7_fgets(char *s, int n, FILE *iop) 
+{
+    register int c;
+    register char *cs;
+
+    cs = s;
+    while (--n > 0 && (c = getc(iop)) != EOF) {
+        if ((*cs++ = c) != '\n')
+            break;
+    }
+    *cs = '\0';
+
+    return (c == EOF && cs == s) ? NULL : s;
+}
---
+++ ch7/ch7_fgets.h
@@ -0,0 +1,13 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ch7_fgets.h
+ *      Template header file.
+ *
+ */
+
+#ifndef CH7_FGETS_H
+#define CH7_FGETS_H
+
+char *fgets(char *, int, FILE *);
+
+#endif /* CH7_FGETS_H */
