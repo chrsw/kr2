88ef78e3bd68d131d2251c22adc79180ad1fbd88
Chris Williams <chrsw@users.noreply.github.com>
2024-01-07 19:16:43 -0500

    Add skeleton for exercise 7-8.

---
+++ ch7/prp.c
@@ -0,0 +1,48 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *      Vim modeline for consistent editor settings across files.
+ *
+ * prp.c
+ *      Print a set of files, starting each new one on a new page.
+ *      Solution to exercise 7-8.
+ *
+ * Description:
+ *      Exercise text:
+ *      Write a program to print a set of files, starting each new one
+ *      on a new page, with a title and running page count for each file.
+ *
+ *      Print a set of files where each file only appears on one page or
+ *      This excercise basically asks the reader to write a simplified
+ *      version of the unix 'pr' command.
+ *
+ * Input:
+ *      A list of files named on the command line.
+ *
+ * Output:
+ *      What output does this program generate? stdout, stderr, files, etc.
+ *
+ * Design:
+ *      Details about the design, theory and options taken for the
+ *      implemented solution.
+ * 
+ * Implementation:
+ *      Details on how the code you're reading implements the design.
+ *
+ * Build:
+ *      $ gcc -o prp prp.c
+ *
+ * Run:
+ *      $ ./prp <file1> <file2> <file3> ... <fileN>
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include <stdio.h>
+
+/* prp:  print a set of files, split file into unique pages */
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
