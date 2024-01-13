99c1cffce36f59ac5aed91894c4a3f752d53773b
Chris Williams <chrsw@users.noreply.github.com>
2024-01-12 23:51:21 -0500

    Solution 7-4: Start solution.

---
+++ ch7/ch7_scanf.c
@@ -0,0 +1,45 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * ch7_scanf.c
+ *      Private version of scanf.c
+ *
+ * Description:
+ *      Exercise 7-4: Write a private version of scanf() analogous to
+ *      minprintf() from the previous section. See minprintf.c in this
+ *      directory for what that entails.
+ *
+ * Input:
+ *      Format string and pointers to place data.
+ *
+ * Output:
+ *      Data specified by format string and input from stdin.
+ *
+ * Returns:
+ *      Number of objects successfully converted.
+ *
+ * Design:
+ *      Details about the design, theory and options taken for the
+ *      implemented solution.
+ * 
+ * Implementation:
+ *      Details on how the code you're reading implements the design.
+ *
+ * Build:
+ *      How to build this program or a build example (incl make targets).
+ *
+ * Run:
+ *      An example of how this program should be run.
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include "ch7_scanf.h"
+
+/* ch7_scanf:  formatted input, chapter 7 version */
+int ch7_scanf(char *format, ...)
+{
+
+    return 0;
+}
