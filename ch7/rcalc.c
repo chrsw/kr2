13ff80c2ae8722203a8c63532f851bb0ea920370
Chris Williams <chrsw@users.noreply.github.com>
2024-01-13 19:38:09 -0500

    rcalc: Rudimentary calculator example, Section 7.4

---
+++ ch7/rcalc.c
@@ -0,0 +1,32 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *      Vim modeline for consistent editor settings across files.
+ *
+ * rcalc.c
+ *      Rudimentary calculator.
+ *
+ * Description:
+ *      Excerpt from Section 7.4
+ *
+ * Build:
+ *      $ gcc -o rcalc rcalc.c
+ *
+ * Run:
+ *      $ ./rcalc
+ *
+ *      Crtl-D to exit
+ *
+ */
+
+#include <stdio.h>
+
+/* rudimentary calculator */
+int main(int argc, char *argv[])
+{
+
+    double sum, v;
+    
+    sum = 0;
+    while (scanf("%lf", &v) == 1)
+        printf("\t%.2f\n", sum += v);
+    return 0;
+}
