baaca1053b8266b6497c7ec2e7cf9b4bc7b8c527
Chris Williams <chrsw@users.noreply.github.com>
2024-01-14 20:38:07 -0500

    ch7_scanf: Add header file for multi-file apps.

---
+++ ch7/ch7_scanf.h
@@ -0,0 +1,12 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * template.h
+ *      Template header file.
+ *
+ */
+
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+
+int ch7_scanf(char *, ...);
+#endif /* TEMPLATE_H */
