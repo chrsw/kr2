adf06ed72a003b81ced1a0f9e1e77504320953c2
Chris Williams <chrsw@users.noreply.github.com>
2024-02-04 19:45:59 -0500

    Solution 8-4: fseek() compiles and calls lseek() syscall
    A small step towards the solution for Exercise 8-4. This version uses
    the custom "syscalls.h" from the text and used in earlier exercises.
    This commit also calls the lseek() system call. Does not do anything
    yet.

--- ch8/ch8_fseek.c
+++ ch8/ch8_fseek.c
@@ -9,7 +9,7 @@
  *
  *          int fseek(FILE *fp, long offset, int origin)
  *
- *      is identical to lseek except that fp i sa file pointer instead of a
+ *      is identical to lseek except that fp is a file pointer instead of a
  *      file descriptor and the return value is an int status not a position.
  *      Write fseek. Make sure that your fseek coordinates properly with the
  *      buffering done for the other functions of the library.
@@ -18,10 +18,16 @@
  *      File pointer, offset to move, 
  *
  * Output:
- *      Operation status: success or error type.
+ *      Operation status: success or error (-1).
+ *      man pages for fseek(2) say on error -1 is returned and errno is set
+ *      indicating the type of error. But for the purpose of this exercise
+ *      setting of errno will be omitted.
  *
  * Design:
- *      TBD
+ *      The basic idea for ch8_fseek() is to check the arguments and call
+ *      the system call lseek() and check the return value. This example will
+ *      have to use the chapter 8 version of stdio.h instead of the system
+ *      version.
  * 
  * Implementation:
  *      TBD
@@ -35,12 +41,21 @@
  */
 
 
-#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "stdio.h"
+
 
 int ch8_fseek(FILE *fp, long offset, int origin);
 
 int ch8_fseek(FILE *fp, long offset, int origin)
 {
+    off_t offs;
 
+    /* first check the file pointer is valid */
+    if (fp == NULL) 
+        return -1;
+
+    offs = lseek(fp->fd, offset, origin); 
     return 0;
 }
