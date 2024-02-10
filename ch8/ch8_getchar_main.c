75d318fe362e5f714ea02b26e86cae09692f8a6a
Chris Williams <chrsw@users.noreply.github.com>
2024-02-10 16:52:59 -0500

    Update chapter 8 getchar test code comments.

--- ch8/ch8_getchar_main.c
+++ ch8/ch8_getchar_main.c
@@ -4,7 +4,7 @@
  *      Test chapter 8 stdio.h
  *
  * Build:
- *      $ gcc -o ch8_getchar_main fillbuf.c ch8_getchar_main.c
+ *      $ gcc -o ch8_getchar_main ch8_fillbuf.c ch8_getchar_main.c
  *
  * Notes:
  *      After running this code in a GDB session, the line:
@@ -15,6 +15,10 @@
  *      anything.
  *      The C Standard Library runtime is supposed to set up _iob[]'s so 
  *      they are linked to the actual files, but that isn't happening here.
+ *
+ * TODO:
+ *      The note above might be outdated. Check accuracy and update if
+ *      necessary.
  */
 
 #include "ch8_stdio.h"
