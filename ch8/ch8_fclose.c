b77b8df46938df523e2e97a32a8b575d80d9094c
Chris Williams <chrsw@users.noreply.github.com>
2024-02-25 16:24:50 -0500

    fclose: Rename to ch8_fclose.

---
+++ ch8/ch8_fclose.c
@@ -0,0 +1,32 @@
+/* vim:ts=4:sw=4:et:so=10:
+ *
+ * ch8_fclose.c
+ *      Close a stream.
+ *
+ * Description:
+ *      Simple fclose() implementation for part of Exercise 8-3.
+ *      
+ * Returns:
+ *      0, error handling not implemented. 
+ *
+ * Build:
+ *      $ gcc -c fclose.c
+ *      - or example app -
+ *      $ gcc -o app fflush.c fclose.c app.c
+ *
+ */
+
+#include <stdlib.h>
+#include "syscalls.h"
+#include "ch8_stdio.h"
+#include "ch8_fflush.h"
+
+
+int ch8_fclose(FILE *fp) {
+
+    ch8_fflush(fp);
+    if (fp->base != NULL)
+        free(fp->base);
+    close(fp->fd);
+    return 0;
+}
