df9559e3c33558b8214e0c69587982b04c6366d3
Chris Williams <chrsw@users.noreply.github.com>
2024-03-03 18:40:13 -0500

    Add dirent.h header from Chapter 8, section 6.

---
+++ ch8/dir/dirent.h
@@ -0,0 +1,29 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * dirent.h
+ *      System-independent access to the name and inode number in a
+ *      directory entry.
+ *
+ */
+
+#ifndef DIRENT_H
+#define DIRENT_H
+
+
+#define NAME_MAX    14  /* longest filename component: system-dependent */
+
+typedef struct {            /* portable directory entry */
+    long ino;               /* inode number */
+    char name[NAME_MAX+1];  /* name + '\0' terminator */
+} Dirent;
+
+typedef struct {            /* minimal DIR: no buffering, etc. */
+    int fd;                 /* file descriptor for directory */
+    Dirent d;               /* the directory entry */
+} DIR;
+
+DIR *opendir(char *dirname);
+Dirent *readdir(DIR *dfd);
+void closedir(DIR *dfd);
+
+#endif /* DIRENT_H */
