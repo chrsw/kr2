dfd82bd5c13f16fe881b02f42639e209a09a2169
Chris Williams <chrsw@users.noreply.github.com>
2024-03-10 23:37:54 -0400

    Solution 8-5: main calls finfo instead of ftime.

--- ch8/dir/ex8-5.c
+++ ch8/dir/ex8-5.c
@@ -1,39 +1,36 @@
 /* vim:ts=4:sw=4:et:so=10:ls=2:
  *
  * ex8-5.c
- *      Main entry point for ftime app.
+ *      Main entry point for finfo app.
  *
  * Description:
- *      Print file modification times recursively.
+ *      Print file information recursively.
  *
  * Input:
  *      Optional directory name.
  *
  * Output:
- *      File names and modification time on standard output.
+ *      File names and other info on standard output.
  *
  * Build:
- *      $ gcc -Wall -Wextra -Wpedantic -o ftime ex8-5.c ftime.c dirwalk.c
+ *      $ gcc -Wall -Wextra -Wpedantic -o finfo ex8-5.c finfo.c dirwalk.c
  *
  * Run:
- *      $ ./ftime
- *
- * Notes:
- *      Helpful information for anyone to have who is maintaining this code.
+ *      $ ./finfo
  *
  */
 
 
-void ftime(char *name);
+void finfo(char *name);
 
 
 /* print file times */
 int main(int argc, char *argv[])
 {
     if (argc == 1)
-        ftime(".");
+        finfo(".");
     else
         while (--argc > 0)
-            ftime(*++argv);
+            finfo(*++argv);
     return 0;
 }
