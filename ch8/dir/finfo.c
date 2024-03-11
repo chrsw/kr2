93faabf87fd6043de0e82ecc68308fb67de3e8d0
Chris Williams <chrsw@users.noreply.github.com>
2024-03-10 23:19:05 -0400

    Solution 8-5: Baseline of finfo(), start from ftime().

---
+++ ch8/dir/finfo.c
@@ -0,0 +1,45 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *      
+ * ftime.c
+ *      Show file modification time.
+ *
+ * Description:
+ *      Part of the solution to Exercise 8-5.
+ *
+ * Input:
+ *      Optional directory name.
+ *
+ * Build:
+ *      $ gcc -c ftime.c
+ *
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+
+
+void dirwalk(char *dir, void(*fcn)(char *));
+
+
+/* ftime:  print modification time of file "name" */
+void ftime(char *name)
+{
+    struct stat stbuf;
+
+    if (stat(name, &stbuf) == -1) {
+        fprintf(stderr, "fsize: can't access %s\n", name);
+        return;
+    }
+    if ((stbuf.st_mode & S_IFMT) == S_IFDIR)
+        dirwalk(name, ftime);
+    printf("%20s\t\t%s", name, ctime(&stbuf.st_mtime));
+}
