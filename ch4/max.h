7a49306c3f049871c686eff1c29b7b7d98950f72
Chris Williams <chrsw@users.noreply.github.com>
2024-06-01 23:34:02 -0400

    Add max() macro from Section 4.11.2

---
+++ ch4/max.h
@@ -0,0 +1,13 @@
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * max.h
+ *      Return the maximum of A, B
+ *
+ */
+
+#ifndef MAX_H
+#define MAX_H 
+
+#define max(A,B)  ((A > B) ? (A) : (B))
+
+#endif /* MAX_H */
