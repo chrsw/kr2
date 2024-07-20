6238e527130a4809f35e7a0467f00fee2caa3247
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-07-20 17:49:54 -0400

    Solution 2-7: Description, formatting, test input.

--- ch2/ex2-7.c
+++ ch2/ex2-7.c
@@ -1,17 +1,44 @@
-/* Exercise 2-7 */
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * ex2-7.c
+ *      Invert n bits in x starting a position p.
+ *
+ * Description:
+ *      Write a function invert(x,p,n) that returns x with the n bits that
+ *      begin at position p set to the rightmost n bits of y, leaving the
+ *      other bits unchanged.
+ *
+ * Input:
+ *      This file has the example input hard coded.
+ *
+ * Output:
+ *      Prints the input value and output value to standard output.
+ *
+ * Design:
+ *      See invert.c
+ *
+ * Build:
+ *      $ gcc -o ex2-7 ex2-7.c invert.c
+ *
+ * Run:
+ *      $ ./ex2-7
+ *
+ */
+
 
 #include <stdio.h>
 #include "invert.h"
 
 int main(int argc, char *argv[])
 {
-    unsigned int x = 0x1111;
+    unsigned int x = 0xf0001110;
     int p = 8;
     int n = 4;
-    unsigned int i = invert(x,p,n);
+    unsigned int i;
+    i = invert(x,p,n);
     
-    printf("       x = %x\n", x);
-    printf("inverted = %x\n", i);
+    printf("   input = %08x\n", x);
+    printf("inverted = %08x\n", i);
 
     return 0;
 }
