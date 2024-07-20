5bcb3700462a23aa06af12387a6852d82119a5ae
Chris Williams <1463243+chrsw@users.noreply.github.com>
2024-07-20 18:12:00 -0400

    Solution 2-7
    Add description of invert function.
    Simplify (but not shorted) code.
    Add file header.

--- ch2/invert.c
+++ ch2/invert.c
@@ -1,35 +1,59 @@
-/* invert:  invert n bits starting at position p in x */
+/* vim:ts=4:sw=4:et:so=10:ls=2:
+ *
+ * invert.c
+ *      Invert n bits in x starting a position p.
+ *
+ * Description:
+ *      Write a function invert(x,p,n) that returns x with the n bits that
+ *      begin at position p set to the rightmost n bits of y, leaving the
+ *      other bits unchanged.
+ *
+ * Build:
+ *      See main driver in ex2-7.c.
+ *
+ * Run:
+ *      $ ./ex2-7
+ *
+ */
+
+
+
 #include <stdio.h>
 #include "invert.h"
 
+/* invert:  invert n bits starting at position p in x */
 unsigned int invert(unsigned x, int p, int n)
 {
-    unsigned int r = 0;
-    unsigned int i = 0;
-    unsigned int z = 0;
+    unsigned int result = 0;
+    unsigned int inverted = 0;
     unsigned int ones = 0;
     unsigned int zeros = 0;
 
+    /* create the inverted bits in LSBs */
+    inverted = (x >> (p-n)) & ~(~0 << n); 
+    inverted = ~inverted;
+    /* ~((x >> (p-n)) & ~(~0 << n)) */ 
 
-    /* create a word of inverted bits from the correct posistion */
-    i = (x >> (p-n)) & ~(~0 << n); 
-    i = ~i;
-
-    /* create n ones */
+    /* create n ones in LSBs */
     ones = ~0;
     ones <<= n;
     ones = ~ones;
+    /* (~(~0 << n)) */
 
-    i &= ones;
-
+    /* clear all the non-n inverted bits */
+    /* leaving only inverted bits in LSBs */
+    inverted &= ones;
+    inverted <<= p;
+    /* (& ones) << p */
 
     /* fill the specified n bits at position p with zeros */
-    i <<= p;
     ones <<= p;
     zeros = ~ones;
 
+    /* clear the n bits in position p */ 
+    result = x & zeros;
+
     /* compute the result */
-    r = x & zeros;
-    r = r | i;
-    return r;
+    result |= inverted;
+    return result ;
 }
