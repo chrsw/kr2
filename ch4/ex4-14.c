ad6c766d451471c57676f02a47fe379442bae0bd
Chris Williams <1463243+chrsw@users.noreply.github.com>
2025-05-26 09:03:30 -0400

    Solution 4-14 various cleanups
    Add build instruction
    Make sure swap() is undefined
    Better swap() macro formatting
    Don't use MAX_INT from limits.h
    Better explanation of output

--- ch4/ex4-14.c
+++ ch4/ex4-14.c
@@ -3,25 +3,28 @@
  *		Define a macro swap(t,x,y) that interchanges two arguments of
  * 		type t. (Block structure will help.)
  *
+ * Build:
+ * 		$ gcc -o ex4-14 ex4-14.c
  */
 
 #include <stdio.h>
-#include <limits.h>
 
-#define swap(t,x,y) do { \
-					t temp;\
+#undef swap
+#define swap(t,x,y) do {     \
+					t temp;  \
 					temp = x;\
-					x = y;\
+					x = y;   \
 					y = temp;\
 				} while (0);
 
 int main(void) {
 
-	int x = INT_MAX;
+	int x = 111;
 	int y = 0;
-	printf("x = %d\ny = %d\n", x, y);
-	printf("swapping...\n");
+	printf("orignal values:\n");
+	printf("x = %d\ty = %d\n", x, y);
+	printf("after swapping with swap(t,x,y) macro:\n");
 	swap(int, x, y);
-	printf("x = %d\ny = %d\n", x, y);
+	printf("x = %d\ty = %d\n", x, y);
 	return 0;
 }
