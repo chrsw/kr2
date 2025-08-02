d893e4ea5dda54f452c60f2702447077d740fc7f
Chris Williams <1463243+chrsw@users.noreply.github.com>
2025-08-01 22:38:06 -0400

    Solution 5-7: Start of exercise

---
+++ ch5/ex5-7_readlines.c
@@ -0,0 +1,6 @@
+/* ex5-7_readlines.c 
+ *
+ * Rewrite readlines() to store lines in an array supplied by main(), rather
+ * than calling alloc() to maintain storage. How much faster is the program?
+ *
+ */
