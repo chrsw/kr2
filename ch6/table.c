5d09f7a07e8117db91d336e4973d739299bcdd98
Chris Williams <1463243+chrsw@users.noreply.github.com>
2025-12-30 20:37:12 -0500

    Table lookup code from Section 6.6

---
+++ ch6/table.c
@@ -0,0 +1,61 @@
+/* 
+ * table.c Code for table lookup package from Section 6.6 Table Lookup
+ *
+ * Build:
+ *   gcc -c table.c
+ */
+
+#include <string.h>
+#include <stdlib.h>
+
+struct nlist {                /* table entry */
+    struct nlist *next;   /* next entry in chain */
+    char *name;          /* defined name */
+    char *defn;          /* replacement text */
+};
+
+#define HASHSIZE 101
+
+static struct nlist *hashtab[HASHSIZE]; /* pointer table */
+
+
+/* hash: form hash value for string s */
+unsigned hash(char *s) {
+    unsigned hashval;
+    for (hashval = 0; *s != '\0'; s++)
+        hashval = *s + 31 * hashval;
+    return hashval % HASHSIZE;
+}
+
+
+/* lookup: look for s in hashtab */
+struct nlist *lookup(char *s) {
+    struct nlist *np;
+
+    for (np = hashtab[hash(s)]; np != NULL; np = np->next) {
+        if (strcmp(s, np->name) == 0)
+            return np; /* found */
+    }
+    return NULL; /* not found */
+}
+
+
+/* install: install (name, defn) in hashtab */
+struct nlist *install(char *name, char *defn) {
+    struct nlist *np;
+    unsigned hashval;
+
+    if ((np = lookup(name)) == NULL) { /* not found */
+        np = (struct nlist *) malloc(sizeof(*np));
+        if (np == NULL || (np->name = strdup(name)) == NULL)
+            return NULL;
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else { /* already there */
+        free((void *) np->defn); /* free previous definition */
+    }
+    if ((np->defn = strdup(defn)) == NULL)
+        return NULL;
+    return np;
+}
