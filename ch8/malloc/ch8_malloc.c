6a871df6ddd0d2f5f56d67990d68005f9b145e7a
Chris Williams <1463243+chrsw@users.noreply.github.com>
2026-01-04 23:55:14 -0500

    More cleanups in ch8 malloc()/free().

--- ch8/malloc/ch8_malloc.c
+++ ch8/malloc/ch8_malloc.c
@@ -6,7 +6,9 @@
  * Description:
  *      Excerpt from K&R2, Chapter 8, Section 7.
  *      Minor name changes to avoid conflicts with the
- *      system malloc() and free(). 
+ *      system malloc() and free(). Additional changes needed
+ *      so the code builds without warnings or problems on
+ *      modern systems.
  *      ch8_malloc(), ch8_free() and morecore() implement a
  *      simple storage allocator using a linked list to keep
  *      track of memory blocks.
@@ -19,7 +21,6 @@
 #include <unistd.h>
 #include "ch8_malloc.h"
 
-#define NALLOC 1024
 
 typedef long Align;                     /* for alignment to long boundary */
 
@@ -33,7 +34,6 @@ union header {                          /* block header */
 
 typedef union header Header;
 
-static Header *morecore(unsigned);
 
 static Header base;                     /* empty list to get started */
 static Header *freep = NULL;            /* start of free list */
@@ -43,7 +43,7 @@ static Header *freep = NULL;            /* start of free list */
 void *ch8_malloc(unsigned nbytes)
 {
     Header *p, *prevp;
-
+    Header *morecore(unsigned);
     unsigned nunits;
 
     nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
@@ -60,8 +60,8 @@ void *ch8_malloc(unsigned nbytes)
                 p += p->s.size;
                 p->s.size = nunits;
             }
-        freep = prevp;
-        return (void *)(p+1);
+            freep = prevp;
+            return (void *)(p+1);
         }
         if (p == freep)                     /* wrapped around free list */
             if ((p = morecore(nunits)) == NULL)
@@ -71,6 +71,7 @@ void *ch8_malloc(unsigned nbytes)
 }
 
 
+#define NALLOC 1024                         /* minimum #units to request */
 
 /* morecore:  ask system for more memory */
 static Header *morecore(unsigned nu)
@@ -94,7 +95,8 @@ static Header *morecore(unsigned nu)
 void ch8_free(void *ap)
 {
     Header *bp, *p;
-    bp = (Header *)ap - 1;
+
+    bp = (Header *)ap - 1;              /* point to block header */
     for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
         if (p >= p->s.ptr && (bp > p || bp < p->s.ptr))
             break;       /* freed block at start or end of arena */
