253241226fb8594298708a247e0b7e38854498a6
Chris Williams <1463243+chrsw@users.noreply.github.com>
2026-01-16 21:31:12 -0500

    Solution 8-2: Exercise text

---
+++ ch8/ex8-2.c
@@ -0,0 +1,39 @@
+/*
+ * ex8-2.c
+ *      Solution for Exercise 8-2.
+ *
+ * Description:
+ *      Rewrite fope() and _fillbuf() with fields instead of explicit bit
+ *      operations. Compare code size and execution speed.
+ *
+ * Input:
+ *      Describe the expected input.
+ *
+ * Output:
+ *      What output does this program generate? stdout, stderr, files, etc.
+ *
+ * Design:
+ *      Details about the design, theory and options taken for the
+ *      implemented solution.
+ * 
+ * Implementation:
+ *      Details on how the code you're reading implements the design.
+ *
+ * Build:
+ *      How to build this program or a build example (incl make targets).
+ *
+ * Run:
+ *      An example of how this program should be run.
+ *
+ * Notes:
+ *      Helpful information for anyone to have who is maintaining this code.
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
