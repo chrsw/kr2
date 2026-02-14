0339718f6916caf308cc9eb51aafb4e977fdfc0f
Chris Williams <1463243+chrsw@users.noreply.github.com>
2026-02-13 21:54:47 -0500

    Solution 1-24: Add initial solution source.

---
+++ ch1/ex1-24.c
@@ -0,0 +1,116 @@
+/*
+ * ex1-23.c
+ *      Write a program to check a C program for rudimentary syntax errors
+ *      like unbalanced parentheses, brackets and braces. Don't forget about
+ *      quotes, both single and double, escape sequences and comments.
+ *
+ * Description:
+ *      Starting with the solution from 1-23 and going from there. That
+ *      solution strips comments from C source text.
+ *
+ * Input:
+ *      A C source file.
+ *
+ * Output:
+ *
+ * Design:
+ * 
+ * Implementation:
+ *      Details on how the code you're reading implements the design.
+ *
+ * Build:
+ *      $ gcc -o 1-24 1-24.c
+ *
+ * Run:
+ *      $ ./ex1-24 < file.c
+ *
+ * Notes:
+ *      Only the most basic checking is included in this solution.
+ *      See ex1-24.txt for more details.
+ *
+ */
+
+#include <stdio.h>
+
+
+#define IN 1        /* inside a word */
+#define OUT 0       /* outside a word */
+
+#define NORMAL   0      /* normal, non-comment text, print */
+#define IN_SLASH 2      /* in possible start of comment block */      
+#define IN_BLOCK 3      /* in comment block */
+#define OUT_STAR 4      /* possibly exiting comment block */
+
+
+int main(void) {
+
+    int c, state;
+    int pc = 0; 
+
+    int oparens,         /* counts */
+        obraces,         
+        obrackets, 
+        cparens,
+        cbraces,
+        cbrackets,
+        squotes,
+        dquotes,
+        escapes = 0;
+    
+    int invalid = 0;    
+    state = NORMAL;
+    
+    while ((c = getchar()) != EOF ) {
+        if (state == NORMAL) {
+            if (c == '/') {
+                state = IN_SLASH;
+                pc = c;
+                continue;
+            }
+            if (c == '\(') oparens++;
+            if (c == ')') cparens++;
+            if (c == '{') obraces++;
+            if (c == '}') cbraces++;
+            if (c == '[') obrackets++;
+            if (c == ']') cbrackets++;
+            if (c == '\'') squotes++;
+            if (c == '\"') dquotes++;
+            if (c == '\"') escapes++;
+        }     
+        if (state == IN_SLASH) {
+            if (c == '*') {
+                state = IN_BLOCK;
+            } else
+                state = NORMAL;
+        }
+        if (state == IN_BLOCK) {
+            if (c == '*') {
+                state = OUT_STAR;
+                continue;
+            }
+        }
+        if (state == OUT_STAR) {
+            if (c == '/') {
+                state = NORMAL;
+                continue;
+            } else
+                state = IN_BLOCK;
+        }
+        if (state == NORMAL) {
+            //if (pc != 0 ) putchar(pc);
+            pc = 0;
+        }
+    }
+
+    if ((oparens != cparens) ||
+        (obraces != cbraces) ||
+        (obrackets != cbrackets))
+        invalid = 1;
+
+    if (((squotes % 2) == 1) ||
+        ((dquotes % 2) == 1) ||
+        ((escapes % 2) == 1))
+        invalid = 1;
+
+    return invalid;
+}
