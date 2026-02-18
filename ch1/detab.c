c1c1b3b371c52651ab34247acdadf64e876bee44
Chris Williams <1463243+chrsw@users.noreply.github.com>
2026-02-17 20:19:57 -0500

    Fix minor typo in detab.c

--- ch1/detab.c
+++ ch1/detab.c
@@ -5,8 +5,8 @@
  * Description:
  *      Write a program detab that replaces tabs in the input with the proper
  *      number of blanks to space to the next tab stop. Assume a fixed set
- *      of tab stops, say ever n columns. Should n be a variable or a symbolic
- *      parameter?
+ *      of tab stops, say every n columns. Should n be a variable or a
+ *      symbolic parameter?
  *      
  * Input:
  *      Text input with line breaks. Lines can't be too long.
