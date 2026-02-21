d6f063cf1c67b907006edb6e69eb3298bf9cb94e
Chris Williams <1463243+chrsw@users.noreply.github.com>
2026-02-21 15:20:08 -0500

    Solution 1-1: Add solution files

---
+++ ch1/ex1-1a.c
@@ -0,0 +1,16 @@
+/*
+ * ex1-1c.c	
+ *      Run the "hello, world" program on your system. Experiment with
+ *      leaving out parts of the program, to see what error messages you
+ *      get.
+ *
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world");
+
+    return 0;
+}
